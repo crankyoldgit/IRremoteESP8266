6d556bb75bd622b66899c9b234cca0dbf0f0f1d8
Sillyfrog <sillyfrog@users.noreply.github.com>
2016-11-08 12:34:50 +1000

    Improve the comments in the example and be more explicit.

--- examples/TurnOnDaikinAC/TurnOnDaikinAC.ino
+++ examples/TurnOnDaikinAC/TurnOnDaikinAC.ino
@@ -11,10 +11,17 @@ void setup(){
 
 void loop(){
   Serial.println("Sending...");
+
+  // Set up what we want to send. See IRDaikinESP.cpp for all the options.
+  dakinir.on();
   dakinir.setFan(1);
   dakinir.setMode(DAIKIN_COOL);
   dakinir.setTemp(25);
+  dakinir.setSwingVertical(0);
+  dakinir.setSwingHorizontal(0);
+
+  // Now send the IR signal.
   dakinir.send();
+
   delay(5000);
 }
-
