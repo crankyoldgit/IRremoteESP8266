87d9203e0d8600915808bcadb8988e776cb343f3
crankyoldgit <david@xyzzy.com.au>
2017-03-17 22:23:34 +1000

    Fix a bug where IRtimer would incorrectly detect a wrap condition. e.g. It thought when start and now were the same, that the micros() value had wrapped.
    This manifested in mark() where .elapsed() was invoked immediately
    after object creation. First uncovered in Issue #118.

--- IRremoteESP8266.cpp
+++ IRremoteESP8266.cpp
@@ -111,7 +111,7 @@ void IRtimer::reset() {
 
 uint32_t IRtimer::elapsed() {
   uint32_t now = micros();
-  if (start < now)  // Check if the system timer has wrapped.
+  if (start <= now)  // Check if the system timer has wrapped.
     return (now - start);  // No wrap.
   else
     return (0xFFFFFFFF - start + now);  // Has wrapped.
