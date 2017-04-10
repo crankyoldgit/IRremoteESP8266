28102b833c2ed8c0f367b7114d242d503a4b33b2
David Conran <crankyoldgit@users.noreply.github.com>
2017-04-10 15:52:42 +1000

    Try to make IRrecvDumpV2 more newbie proof.
    * Include a link to an example circuit.
    * Make the GPIO pin used in the code match the circuit diagram.
    * Use a faster baud rate to help capture codes better (less time printing).
    * Use a slightly safer Serial port setup.
    * Some code-style cleanups.

--- examples/IRrecvDumpV2/IRrecvDumpV2.ino
+++ examples/IRrecvDumpV2/IRrecvDumpV2.ino
@@ -1,33 +1,33 @@
 /*
  * IRremoteESP8266: IRrecvDumpV2 - dump details of IR codes with IRrecv
  * An IR detector/demodulator must be connected to the input RECV_PIN.
+ * Example circuit diagram:
+ *  https://github.com/markszabo/IRremoteESP8266/wiki#ir-receiving
  * Version 0.1 Sept, 2015
  * Based on Ken Shirriff's IrsendDemo Version 0.1 July, 2009, Copyright 2009 Ken Shirriff, http://arcfn.com
  */
 
 #include <IRremoteESP8266.h>
 
-int RECV_PIN = 2; //an IR detector/demodulator is connected to GPIO pin 2
+// An IR detector/demodulator is connected to GPIO pin 14(D5 on a NodeMCU board).
+int RECV_PIN = 14;
 
 IRrecv irrecv(RECV_PIN);
 
-void  setup ( )
-{
-  Serial.begin(9600);   // Status message will be sent to PC at 9600 baud
+void setup() {
+  Serial.begin(115200, SERIAL_8N1, SERIAL_TX_ONLY);  // Status message will be sent to the PC at 115200 baud
   irrecv.enableIRIn();  // Start the receiver
 }
 
 //+=============================================================================
 // Display IR code
 //
-void  ircode (decode_results *results)
-{
+void ircode(decode_results *results) {
   // Panasonic has an Address
   if (results->decode_type == PANASONIC) {
     Serial.print(results->panasonicAddress, HEX);
     Serial.print(":");
   }
-
   // Print Code
   Serial.print(results->value, HEX);
 }
@@ -35,8 +35,7 @@ void  ircode (decode_results *results)
 //+=============================================================================
 // Display encoding type
 //
-void  encoding (decode_results *results)
-{
+void encoding(decode_results *results) {
   switch (results->decode_type) {
     default:
     case UNKNOWN:      Serial.print("UNKNOWN");       break ;
@@ -60,8 +59,7 @@ void  encoding (decode_results *results)
 //+=============================================================================
 // Dump out the decode_results structure.
 //
-void  dumpInfo (decode_results *results)
-{
+void dumpInfo (decode_results *results) {
   if (results->overflow) {
     Serial.println("IR code too long. Edit IRremoteInt.h and increase RAWBUF");
     return;
@@ -83,8 +81,7 @@ void  dumpInfo (decode_results *results)
 //+=============================================================================
 // Dump out the decode_results structure.
 //
-void  dumpRaw (decode_results *results)
-{
+void dumpRaw(decode_results *results) {
   // Print Raw data
   Serial.print("Timing[");
   Serial.print(results->rawlen-1, DEC);
@@ -113,8 +110,7 @@ void  dumpRaw (decode_results *results)
 //+=============================================================================
 // Dump out the decode_results structure.
 //
-void  dumpCode (decode_results *results)
-{
+void dumpCode (decode_results *results) {
   // Start declaration
   Serial.print("unsigned int  ");          // variable type
   Serial.print("rawData[");                // array name
@@ -142,7 +138,6 @@ void  dumpCode (decode_results *results)
 
   // Now dump "known" codes
   if (results->decode_type != UNKNOWN) {
-
     // Some protocols have an address
     if (results->decode_type == PANASONIC) {
       Serial.print("unsigned int  addr = 0x");
@@ -160,8 +155,7 @@ void  dumpCode (decode_results *results)
 //+=============================================================================
 // The repeating section of the code
 //
-void  loop ( )
-{
+void loop() {
   decode_results  results;        // Somewhere to store the results
 
   if (irrecv.decode(&results)) {  // Grab an IR code
