ca4b2df693592d054f796589b457eb2852e736d2
David Conran <crankyoldgit@users.noreply.github.com>
2017-04-28 14:46:23 +1000

    Upgrade GC, Raw, & Sony to v2.0 spec. Misc other cleanups. (#182)
    - Clean up a lot if misc. issues to meet v2.0 standards.
    - Add strict and specific bit length decoding to Sony.
    - Clean up constants spacing and unsigned-ness.
    - Code & comment style cleanup.
    - Other mostly cosmetic changes.
    - Add "Status" comment so users can know how much they should trust
      something and we can track it.
    - Make sendGC() more readable, hopefully.
    - Clean up decodeDaikin's code. Discovered it's horribly broken. Comment out.

--- IRremoteInt.h
+++ IRremoteInt.h
@@ -41,10 +41,10 @@
 // First MARK is the one after the long gap
 // pulse parameters in usec
 #define COOLIX_BIT_MARK   560U  // Approximately 21 cycles at 38kHz
-#define COOLIX_ONE_SPACE  COOLIX_BIT_MARK * 3
-#define COOLIX_ZERO_SPACE COOLIX_BIT_MARK * 1
-#define COOLIX_HDR_MARK   COOLIX_BIT_MARK * 8
-#define COOLIX_HDR_SPACE  COOLIX_BIT_MARK * 8
+#define COOLIX_ONE_SPACE  COOLIX_BIT_MARK * 3U
+#define COOLIX_ZERO_SPACE COOLIX_BIT_MARK * 1U
+#define COOLIX_HDR_MARK   COOLIX_BIT_MARK * 8U
+#define COOLIX_HDR_SPACE  COOLIX_BIT_MARK * 8U
 #define COOLIX_MIN_GAP    COOLIX_HDR_SPACE + COOLIX_ZERO_SPACE
 
 #define WHYNTER_HDR_MARK             2850U
@@ -55,26 +55,33 @@
 #define WHYNTER_MIN_COMMAND_LENGTH 108000UL  // Completely made up value.
 #define WHYNTER_MIN_GAP WHYNTER_MIN_COMMAND_LENGTH - 2 * (WHYNTER_BIT_MARK + WHYNTER_ZERO_SPACE) - WHYNTER_BITS * (WHYNTER_BIT_MARK + WHYNTER_ONE_SPACE)
 
-#define NEC_HDR_MARK             9000
-#define NEC_HDR_SPACE            4500
-#define NEC_BIT_MARK              560
-#define NEC_ONE_SPACE            1690
-#define NEC_ZERO_SPACE            560
-#define NEC_RPT_SPACE            2250
-#define NEC_RPT_LENGTH              4
+#define NEC_HDR_MARK             9000U
+#define NEC_HDR_SPACE            4500U
+#define NEC_BIT_MARK              560U
+#define NEC_ONE_SPACE            1690U
+#define NEC_ZERO_SPACE            560U
+#define NEC_RPT_SPACE            2250U
+#define NEC_RPT_LENGTH              4U
 #define NEC_MIN_COMMAND_LENGTH 108000UL
 #define NEC_MIN_GAP NEC_MIN_COMMAND_LENGTH - NEC_HDR_MARK - NEC_HDR_SPACE - NEC_BITS * (NEC_BIT_MARK + NEC_ONE_SPACE) + NEC_BIT_MARK
 
-// Timings based on http://www.sbprojects.com/knowledge/ir/sirc.php
-#define SONY_HDR_MARK	  2400
-#define SONY_SPACE      600
-#define SONY_ONE_MARK	  1200 + 50  // Experiments suggest +50 to spec is better.
-#define SONY_ZERO_MARK	600 + 50  // Experiments suggest +50 to spec is better.
-#define SONY_RPT_LENGTH 45000
-#define SONY_MIN_GAP    10000
+#define SHERWOOD_MIN_REPEAT 1U
+
+// Ref:
+//   http://www.sbprojects.com/knowledge/ir/sirc.php
+#define SONY_HDR_MARK         2400U
+#define SONY_SPACE             600U
+#define SONY_ONE_MARK   1200U + 50U  // Experiments suggest +50 is better.
+#define SONY_ZERO_MARK   600U + 50U  // Experiments suggest +50 is better.
+#define SONY_RPT_LENGTH      45000U
+#define SONY_MIN_GAP         10000U
+#define SONY_12_BITS            12U
+#define SONY_15_BITS            15U
+#define SONY_20_BITS            20U
 
 // Sanyo SA 8650B
-// Ref: https://github.com/z3t0/Arduino-IRremote/blob/master/ir_Sanyo.cpp
+// Ref:
+//   https://github.com/z3t0/Arduino-IRremote/blob/master/ir_Sanyo.cpp
 #define SANYO_SA8650B_HDR_MARK          3500U  // seen range 3500
 #define SANYO_SA8650B_HDR_SPACE          950U  // seen 950
 #define SANYO_SA8650B_ONE_MARK          2400U  // seen 2400
@@ -113,38 +120,39 @@
 #define MITSUBISHI_MIN_REPEAT             1U  // Based on marcosamarinho's code.
 
 // Mitsubishi A/C
-// Values were initially obtained from:
+// Ref:
 //   https://github.com/r45635/HVAC-IR-Control/blob/master/HVAC_ESP8266/HVAC_ESP8266.ino#L84
-#define MITSUBISHI_AC_HDR_MARK    3400
-#define MITSUBISHI_AC_HDR_SPACE   1750
-#define MITSUBISHI_AC_BIT_MARK    450
-#define MITSUBISHI_AC_ONE_SPACE   1300
-#define MITSUBISHI_AC_ZERO_SPACE  420
-#define MITSUBISHI_AC_RPT_MARK    440
-#define MITSUBISHI_AC_RPT_SPACE   17100L
+#define MITSUBISHI_AC_HDR_MARK    3400U
+#define MITSUBISHI_AC_HDR_SPACE   1750U
+#define MITSUBISHI_AC_BIT_MARK     450U
+#define MITSUBISHI_AC_ONE_SPACE   1300U
+#define MITSUBISHI_AC_ZERO_SPACE   420U
+#define MITSUBISHI_AC_RPT_MARK     440U
+#define MITSUBISHI_AC_RPT_SPACE  17100UL
 
 
-#define RC5_T1		889
-#define RC5_RPT_LENGTH	46000
+#define RC5_T1           889U
+#define RC5_RPT_LENGTH 46000U
 
-#define RC6_HDR_MARK	2666
-#define RC6_HDR_SPACE	889
-#define RC6_T1		444
-#define RC6_RPT_LENGTH	46000
+#define RC6_HDR_MARK    2666U
+#define RC6_HDR_SPACE    889U
+#define RC6_T1           444U
+#define RC6_RPT_LENGTH 46000U
 
-// http://www.sbprojects.com/knowledge/ir/rcmm.php
-#define RCMM_HDR_MARK 416
-#define RCMM_HDR_SPACE 277
-#define RCMM_BIT_MARK 166
-#define RCMM_BIT_SPACE_0 277
-#define RCMM_BIT_SPACE_1 444
-#define RCMM_BIT_SPACE_2 611
-#define RCMM_BIT_SPACE_3 777
-#define RCMM_RPT_LENGTH 27778
-#define RCMM_MIN_GAP 3360
+// Ref:
+//   http://www.sbprojects.com/knowledge/ir/rcmm.php
+#define RCMM_HDR_MARK      416U
+#define RCMM_HDR_SPACE     277U
+#define RCMM_BIT_MARK      166U
+#define RCMM_BIT_SPACE_0   277U
+#define RCMM_BIT_SPACE_1   444U
+#define RCMM_BIT_SPACE_2   611U
+#define RCMM_BIT_SPACE_3   777U
+#define RCMM_RPT_LENGTH  27778U
+#define RCMM_MIN_GAP      3360U
 // Use a tolerance of +/-10% when matching some data spaces.
-#define RCMM_TOLERANCE 10
-#define RCMM_EXCESS 50
+#define RCMM_TOLERANCE      10U
+#define RCMM_EXCESS         50U
 
 // Sharp period time = 1/38000Hz = 26.316 microseconds.
 // Ref:
@@ -165,104 +173,111 @@
 // Ref:
 //   https://github.com/marcosamarinho/IRremoteESP8266/blob/master/ir_Dish.cpp
 //   http://www.hifi-remote.com/wiki/index.php?title=Dish
-#define DISH_HDR_MARK    400
-#define DISH_HDR_SPACE  6100
-#define DISH_BIT_MARK    400
-#define DISH_ONE_SPACE  1700
-#define DISH_ZERO_SPACE 2800
-#define DISH_RPT_SPACE  6200
-#define DISH_MIN_REPEAT    3
+#define DISH_HDR_MARK    400U
+#define DISH_HDR_SPACE  6100U
+#define DISH_BIT_MARK    400U
+#define DISH_ONE_SPACE  1700U
+#define DISH_ZERO_SPACE 2800U
+#define DISH_RPT_SPACE  6200U
+#define DISH_MIN_REPEAT    3U
 
-// Ref: http://www.remotecentral.com/cgi-bin/mboard/rc-pronto/thread.cgi?26152
-#define PANASONIC_HDR_MARK             3456
-#define PANASONIC_HDR_SPACE            1728
-#define PANASONIC_BIT_MARK              432
-#define PANASONIC_ONE_SPACE            1296
-#define PANASONIC_ZERO_SPACE            432
+// Ref:
+//   http://www.remotecentral.com/cgi-bin/mboard/rc-pronto/thread.cgi?26152
+#define PANASONIC_HDR_MARK             3456U
+#define PANASONIC_HDR_SPACE            1728U
+#define PANASONIC_BIT_MARK              432U
+#define PANASONIC_ONE_SPACE            1296U
+#define PANASONIC_ZERO_SPACE            432U
 #define PANASONIC_MIN_COMMAND_LENGTH 130000UL
 // 130000 - 3456 - 1728 - 48*(432+1296) - 432 = 41440
-#define PANASONIC_MIN_GAP             41440
+#define PANASONIC_MIN_GAP             41440U
 #define PANASONIC_MANUFACTURER       0x2002ULL
 
 
-// Ref: http://www.sbprojects.com/knowledge/ir/jvc.php
-#define JVC_HDR_MARK    8400
-#define JVC_HDR_SPACE   4200
-#define JVC_BIT_MARK     525
-#define JVC_ONE_SPACE   1725
-#define JVC_ZERO_SPACE   525
-#define JVC_RPT_LENGTH 60000
-#define JVC_MIN_GAP    11400  // 60000 - 16 * (1725 + 525) - 8400 - 4200
+// Ref:
+//   http://www.sbprojects.com/knowledge/ir/jvc.php
+#define JVC_HDR_MARK    8400U
+#define JVC_HDR_SPACE   4200U
+#define JVC_BIT_MARK     525U
+#define JVC_ONE_SPACE   1725U
+#define JVC_ZERO_SPACE   525U
+#define JVC_RPT_LENGTH 60000U
+#define JVC_MIN_GAP    11400U  // 60000 - 16 * (1725 + 525) - 8400 - 4200
 
-#define LG_HDR_MARK    8000
-#define LG_HDR_SPACE   4000
-#define LG_BIT_MARK     600
-#define LG_ONE_SPACE   1600
-#define LG_ZERO_SPACE   550
-#define LG_RPT_SPACE   2250
-#define LG_MIN_GAP    20000  // Completely made up figure.
+#define LG_HDR_MARK             8000U
+#define LG_HDR_SPACE            4000U
+#define LG_BIT_MARK              600U
+#define LG_ONE_SPACE            1600U
+#define LG_ZERO_SPACE            550U
+#define LG_RPT_SPACE            2250U
+#define LG_MIN_GAP             20000U  // Completely made up figure.
 #define LG_MIN_MESSAGE_LENGTH 108000UL
-#define LG32_HDR_MARK  4500
+#define LG32_HDR_MARK           4500U
 
-// Ref: http://elektrolab.wz.cz/katalog/samsung_protocol.pdf
-#define SAMSUNG_HDR_MARK   4500
-#define SAMSUNG_HDR_SPACE  4500
-#define SAMSUNG_BIT_MARK    560
-#define SAMSUNG_ONE_SPACE  1690
-#define SAMSUNG_ZERO_SPACE  560
-#define SAMSUNG_RPT_SPACE  2250
-#define SAMSUNG_MIN_GAP   20000  // Completely made up figure.
+// Ref:
+//   http://elektrolab.wz.cz/katalog/samsung_protocol.pdf
+#define SAMSUNG_HDR_MARK             4500U
+#define SAMSUNG_HDR_SPACE            4500U
+#define SAMSUNG_BIT_MARK              560U
+#define SAMSUNG_ONE_SPACE            1690U
+#define SAMSUNG_ZERO_SPACE            560U
+#define SAMSUNG_RPT_SPACE            2250U
+#define SAMSUNG_MIN_GAP             20000U  // Completely made up figure.
 #define SAMSUNG_MIN_MESSAGE_LENGTH 108000UL
 
-#define DISH_BITS 16
+#define DISH_BITS 16U
 
-// Daikin, from https://github.com/mharizanov/Daikin-AC-remote-control-over-the-Internet/tree/master/IRremote
-#define DAIKIN_HDR_MARK	    3650 //DAIKIN_ZERO_MARK*8
-#define DAIKIN_HDR_SPACE	1623 //DAIKIN_ZERO_MARK*4
-#define DAIKIN_ONE_SPACE	1280
-#define DAIKIN_ONE_MARK	    428
-#define DAIKIN_ZERO_MARK	428
-#define DAIKIN_ZERO_SPACE 428
+// Ref:
+//   https://github.com/mharizanov/Daikin-AC-remote-control-over-the-Internet/tree/master/IRremote
+#define DAIKIN_HDR_MARK    3650U  // DAIKIN_ZERO_MARK * 8
+#define DAIKIN_HDR_SPACE   1623U  // DAIKIN_ZERO_MARK * 4
+#define DAIKIN_ONE_SPACE   1280U
+#define DAIKIN_ONE_MARK     428U
+#define DAIKIN_ZERO_MARK    428U
+#define DAIKIN_ZERO_SPACE   428U
+#define DAIKIN_GAP        29000U
 
-//Denon, from https://github.com/z3t0/Arduino-IRremote/blob/master/ir_Denon.cpp
-#define DENON_BITS                   14  // The number of bits in the command
-#define DENON_HDR_MARK              263  // The length of the Header:Mark
-#define DENON_HDR_SPACE             789  // The lenght of the Header:Space
-#define DENON_BIT_MARK              263  // The length of a Bit:Mark
-#define DENON_ONE_SPACE            1842  // The length of a Bit:Space for 1's
-#define DENON_ZERO_SPACE            789  // The length of a Bit:Space for 0's
+// Ref:
+//   https://github.com/z3t0/Arduino-IRremote/blob/master/ir_Denon.cpp
+#define DENON_BITS                   14U  // The number of bits in the command
+#define DENON_HDR_MARK              263U  // The length of the Header:Mark
+#define DENON_HDR_SPACE             789U  // The lenght of the Header:Space
+#define DENON_BIT_MARK              263U  // The length of a Bit:Mark
+#define DENON_ONE_SPACE            1842U  // The length of a Bit:Space for 1's
+#define DENON_ZERO_SPACE            789U  // The length of a Bit:Space for 0's
 #define DENON_MIN_COMMAND_LENGTH 134052UL
 #define DENON_MIN_GAP DENON_MIN_COMMAND_LENGTH - DENON_HDR_MARK - DENON_HDR_SPACE - DENON_BITS * (DENON_BIT_MARK + DENON_ONE_SPACE) - DENON_BIT_MARK
 
-#define KELVINATOR_HDR_MARK	  8990U
-#define KELVINATOR_HDR_SPACE	4490U
-#define KELVINATOR_BIT_MARK	  675U
-#define KELVINATOR_ONE_SPACE	1560U
-#define KELVINATOR_ZERO_SPACE	520U
-#define KELVINATOR_GAP_SPACE	19950U
-#define KELVINATOR_CMD_FOOTER	2U
+#define KELVINATOR_HDR_MARK    8990U
+#define KELVINATOR_HDR_SPACE   4490U
+#define KELVINATOR_BIT_MARK     675U
+#define KELVINATOR_ONE_SPACE   1560U
+#define KELVINATOR_ZERO_SPACE   520U
+#define KELVINATOR_GAP_SPACE  19950U
+#define KELVINATOR_CMD_FOOTER     2U
+
+#define GLOBALCACHE_MAX_REPEAT       50U
+#define GLOBALCACHE_MIN_USEC         80U
+#define GLOBALCACHE_FREQ_INDEX        0U
+#define GLOBALCACHE_RPT_INDEX        GLOBALCACHE_FREQ_INDEX + 1U
+#define GLOBALCACHE_RPT_START_INDEX  GLOBALCACHE_RPT_INDEX + 1U
+#define GLOBALCACHE_START_INDEX      GLOBALCACHE_RPT_START_INDEX + 1U
 
 // Some useful constants
-#define USECPERTICK 50U  // microseconds per clock interrupt tick
-#define RAWBUF 100U // Length of raw duration buffer
-#define HEADER 2U  // Usual nr. of header entries.
-#define FOOTER 2U  // Usual nr. of footer (stop bits) entries.
-#define OFFSET_START 1U  // Usual rawbuf entry to start processing from.
-
+#define USECPERTICK  50U  // microseconds per clock interrupt tick
+#define RAWBUF      100U // Length of raw duration buffer
+#define HEADER        2U  // Usual nr. of header entries.
+#define FOOTER        2U  // Usual nr. of footer (stop bits) entries.
+#define OFFSET_START  1U  // Usual rawbuf entry to start processing from.
 // Marks tend to be 100us too long, and spaces 100us too short
 // when received due to sensor lag.
-#define MARK_EXCESS 100
-
-#define _GAP 5000 // Minimum map between transmissions
-#define GAP_TICKS (_GAP/USECPERTICK)
-
-#define TOLERANCE 25  // default percent tolerance in measurements
-
+#define MARK_EXCESS 100U
+#define TOLERANCE    25U  // default percent tolerance in measurements
 // receiver states
-#define STATE_IDLE     2
-#define STATE_MARK     3
-#define STATE_SPACE    4
-#define STATE_STOP     5
+#define STATE_IDLE    2U
+#define STATE_MARK    3U
+#define STATE_SPACE   4U
+#define STATE_STOP    5U
 
 // information for the interrupt handler
 typedef struct {
@@ -274,30 +289,28 @@ typedef struct {
   // handler. Don't ask why, I don't know. It just does.
   uint16_t rawlen;              // counter of entries in rawbuf.
   uint8_t overflow;             // Buffer overflow indicator.
-}
-irparams_t;
+} irparams_t;
 
 // Defined in IRremote.cpp
 extern volatile irparams_t irparams;
 
 // IR detector output is active low
-#define MARK  0
-#define SPACE 1
+#define MARK  0U
+#define SPACE 1U
 
-#define TOPBIT 0x80000000
-
-#define NEC_BITS 32
-#define SONY_MIN_BITS 12
+#define NEC_BITS           32U
+#define SHERWOOD_BITS      NEC_BITS
+#define SONY_MIN_BITS      SONY_12_BITS
 #define SANYO_SA8650B_BITS 12U
-#define MITSUBISHI_BITS 16
-#define MIN_RC5_SAMPLES 11
-#define MIN_RC6_SAMPLES 1
-#define PANASONIC_BITS 48
-#define JVC_BITS 16
-#define LG_BITS 28
-#define SAMSUNG_BITS 32
-#define WHYNTER_BITS 32U
-#define COOLIX_BITS 24
-#define DAIKIN_BITS 99
+#define MITSUBISHI_BITS    16U
+#define MIN_RC5_SAMPLES    11U
+#define MIN_RC6_SAMPLES     1U
+#define PANASONIC_BITS     48U
+#define JVC_BITS           16U
+#define LG_BITS            28U
+#define SAMSUNG_BITS       32U
+#define WHYNTER_BITS       32U
+#define COOLIX_BITS        24U
+#define DAIKIN_BITS        99U
 
 #endif
