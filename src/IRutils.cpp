ef5ac9412bcdf3f97b89c0da0f0315b713bacbd4
crankyoldgit <david@xyzzy.com.au>
2017-05-08 01:26:10 +1000

    Fix reverseBits() Not sure what I was thinking when I wrote this originally. It certainly didn't reverse the bits. I stepped this version through with pen and paper, it should do the job. Also added preserving the unreversed bits.

--- src/IRutils.cpp
+++ src/IRutils.cpp
@@ -12,13 +12,16 @@
 // Returns:
 //   The reversed bit pattern.
 uint64_t reverseBits(uint64_t input, uint16_t nbits) {
-  uint64_t output = input;
-  for (uint16_t i = 1;
-       i < std::min(nbits, (uint16_t) (sizeof(input) * 8));
-       i++) {
+  if (nbits <= 1)
+    return input;  // Reversing <= 1 bits makes no change at all.
+  // Cap the nr. of bits to rotate to the max nr. of bits in the input.
+  nbits = std::min(nbits, (uint16_t) (sizeof(input) * 8));
+  uint64_t output = 0;
+  for (uint16_t i = 0; i < nbits; i++) {
     output <<= 1;
-    input  >>= 1;
     output |= (input & 1);
+    input >>= 1;
   }
-  return output;
+  // Merge any remaining unreversed bits back to the top of the reversed bits.
+  return (input << nbits) | output;
 }
