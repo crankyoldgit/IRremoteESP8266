eff271fa44711e2dac7d55aac31d6ce9e130554c
crankyoldgit <david@xyzzy.com.au>
2017-05-15 12:51:29 +1000

    Unit tests for Kelvinator A/C. - Unit test coverage for sendKelvinator() and the message construction class. - Add repeats to sendKelvinator(), make it look like a more standard call. - White space and code style cleanups. - Set the default temp for Dry mode. - Minor cleanups.

--- src/ir_Kelvinator.cpp
+++ src/ir_Kelvinator.cpp
@@ -12,7 +12,7 @@
 //    - "I Feel" button & mode.
 //    - Energy Saving mode.
 //    - Low Heat mode.
-//   - Fahrenheit.
+//    - Fahrenheit.
 
 #include "ir_Kelvinator.h"
 #include <algorithm>
@@ -32,6 +32,7 @@
 #define KELVINATOR_GAP_SPACE               19950U
 #define KELVINATOR_CMD_FOOTER                  2U
 
+#define KELVINATOR_POWER                       8U
 #define KELVINATOR_MODE_MASK                0xF8U
 #define KELVINATOR_FAN_OFFSET                  4U
 #define KELVINATOR_BASIC_FAN_MASK uint8_t(0xFFU ^ (3U << KELVINATOR_FAN_OFFSET))
@@ -57,62 +58,69 @@
 // Send a Kelvinator A/C message.
 //
 // Args:
-//   data: An array of KELVINATOR_STATE_LENGTH bytes containing the IR command.
+//   data: An array of bytes containing the IR command.
+//   nbytes: Nr. of bytes of data in the array. (>=KELVINATOR_STATE_LENGTH)
+//   repeat: Nr. of times the message is to be repeated. (Default = 0).
 //
 // Status: STABLE / Known working.
 //
-// Ref:
-//   IRKelvinator.cpp
-void IRsend::sendKelvinator(unsigned char data[]) {
-  uint8_t i = 0;
+void IRsend::sendKelvinator(unsigned char data[], uint16_t nbytes,
+                            uint16_t repeat) {
+  if (nbytes < KELVINATOR_STATE_LENGTH)
+    return;  // Not enough bytes to send a proper message.
+
   // Set IR carrier frequency
   enableIROut(38);
-  // Header #1
-  mark(KELVINATOR_HDR_MARK);
-  space(KELVINATOR_HDR_SPACE);
-  // Data (command)
-  // Send the first command data (4 bytes)
-  for (; i < 4; i++)
+
+  for (uint16_t r = 0; r <= repeat; r++) {
+    // Header #1
+    mark(KELVINATOR_HDR_MARK);
+    space(KELVINATOR_HDR_SPACE);
+    // Data (command)
+    // Send the first command data (4 bytes)
+    uint8_t i;
+    for (i = 0; i < 4; i++)
+      sendData(KELVINATOR_BIT_MARK, KELVINATOR_ONE_SPACE, KELVINATOR_BIT_MARK,
+               KELVINATOR_ZERO_SPACE, data[i], 8, false);
+    // Send Footer for the command data (3 bits (0b010))
     sendData(KELVINATOR_BIT_MARK, KELVINATOR_ONE_SPACE, KELVINATOR_BIT_MARK,
-             KELVINATOR_ZERO_SPACE, data[i], 8, false);
-  // Send Footer for the command data (3 bits (B010))
-  sendData(KELVINATOR_BIT_MARK, KELVINATOR_ONE_SPACE, KELVINATOR_BIT_MARK,
-           KELVINATOR_ZERO_SPACE, KELVINATOR_CMD_FOOTER, 3, false);
-  // Send an interdata gap.
-  mark(KELVINATOR_BIT_MARK);
-  space(KELVINATOR_GAP_SPACE);
-  // Data (options)
-  // Send the 1st option chunk of data (4 bytes).
-  for (; i < 8; i++)
+             KELVINATOR_ZERO_SPACE, KELVINATOR_CMD_FOOTER, 3, false);
+    // Send an interdata gap.
+    mark(KELVINATOR_BIT_MARK);
+    space(KELVINATOR_GAP_SPACE);
+    // Data (options)
+    // Send the 1st option chunk of data (4 bytes).
+    for (; i < 8; i++)
+      sendData(KELVINATOR_BIT_MARK, KELVINATOR_ONE_SPACE, KELVINATOR_BIT_MARK,
+               KELVINATOR_ZERO_SPACE, data[i], 8, false);
+    // Send a double data gap to signify we are starting a new command sequence.
+    mark(KELVINATOR_BIT_MARK);
+    space(KELVINATOR_GAP_SPACE * 2);
+    // Header #2
+    mark(KELVINATOR_HDR_MARK);
+    space(KELVINATOR_HDR_SPACE);
+    // Data (command)
+    // Send the 2nd command data (4 bytes).
+    // Basically an almost identical repeat of the earlier command data.
+    for (; i < 12; i++)
+      sendData(KELVINATOR_BIT_MARK, KELVINATOR_ONE_SPACE, KELVINATOR_BIT_MARK,
+               KELVINATOR_ZERO_SPACE, data[i], 8, false);
+    // Send Footer for the command data (3 bits (B010))
     sendData(KELVINATOR_BIT_MARK, KELVINATOR_ONE_SPACE, KELVINATOR_BIT_MARK,
-             KELVINATOR_ZERO_SPACE, data[i], 8, false);
-  // Send a double data gap to signify we are starting a new command sequence.
-  mark(KELVINATOR_BIT_MARK);
-  space(KELVINATOR_GAP_SPACE * 2);
-  // Header #2
-  mark(KELVINATOR_HDR_MARK);
-  space(KELVINATOR_HDR_SPACE);
-  // Data (command)
-  // Send the 2nd command data (4 bytes).
-  // Basically an almost identical repeat of the earlier command data.
-  for (; i < 12; i++)
-    sendData(KELVINATOR_BIT_MARK, KELVINATOR_ONE_SPACE, KELVINATOR_BIT_MARK,
-             KELVINATOR_ZERO_SPACE, data[i], 8, false);
-  // Send Footer for the command data (3 bits (B010))
-  sendData(KELVINATOR_BIT_MARK, KELVINATOR_ONE_SPACE, KELVINATOR_BIT_MARK,
-           KELVINATOR_ZERO_SPACE, KELVINATOR_CMD_FOOTER, 3, false);
-  // Send an interdata gap.
-  mark(KELVINATOR_BIT_MARK);
-  space(KELVINATOR_GAP_SPACE);
-  // Data (options)
-  // Send the 2nd option chunk of data (4 bytes).
-  // Unlike the commands, definitely not a repeat of the earlier option data.
-  for (; i < KELVINATOR_STATE_LENGTH; i++)
-    sendData(KELVINATOR_BIT_MARK, KELVINATOR_ONE_SPACE, KELVINATOR_BIT_MARK,
-             KELVINATOR_ZERO_SPACE, data[i], 8, false);
-  // Footer
-  mark(KELVINATOR_BIT_MARK);
-  ledOff();
+             KELVINATOR_ZERO_SPACE, KELVINATOR_CMD_FOOTER, 3, false);
+    // Send an interdata gap.
+    mark(KELVINATOR_BIT_MARK);
+    space(KELVINATOR_GAP_SPACE);
+    // Data (options)
+    // Send the 2nd option chunk of data (4 bytes).
+    // Unlike the commands, definitely not a repeat of the earlier option data.
+    for (; i < KELVINATOR_STATE_LENGTH; i++)
+      sendData(KELVINATOR_BIT_MARK, KELVINATOR_ONE_SPACE, KELVINATOR_BIT_MARK,
+               KELVINATOR_ZERO_SPACE, data[i], 8, false);
+    // Footer
+    mark(KELVINATOR_BIT_MARK);
+    space(KELVINATOR_GAP_SPACE * 2);
+  }
 }
 
 IRKelvinatorAC::IRKelvinatorAC(uint16_t pin) : _irsend(pin) {
@@ -127,7 +135,7 @@ void IRKelvinatorAC::stateReset() {
 }
 
 void IRKelvinatorAC::begin() {
-    _irsend.begin();
+  _irsend.begin();
 }
 
 void IRKelvinatorAC::fixup() {
@@ -153,13 +161,11 @@ void IRKelvinatorAC::checksum() {
   for (uint8_t offset = 0; offset < KELVINATOR_STATE_LENGTH; offset += 8) {
     uint8_t sum = KELVINATOR_CHECKSUM_START;
     // Sum the lower half of the first 4 bytes of this block.
-    for (uint8_t i = 0; i < 4; i++) {
+    for (uint8_t i = 0; i < 4; i++)
       sum += (remote_state[i + offset] & 0xFU);
-    }
     // then sum the upper half of the next 3 bytes.
-    for (uint8_t i = 4; i < 7; i++) {
+    for (uint8_t i = 4; i < 7; i++)
       sum += (remote_state[i + offset] >> 4);
-    }
     // Trim it down to fit into the 4 bits allowed. i.e. Mod 16.
     sum &= 0xFU;
     // Place it into the IR code in the top half of the 8th & 16th byte.
@@ -168,15 +174,13 @@ void IRKelvinatorAC::checksum() {
 }
 
 void IRKelvinatorAC::on() {
-    // state = ON;
-    remote_state[0] |= KELVINATOR_POWER;
-    remote_state[8] = remote_state[0];  // Duplicate to the 2nd command chunk.
+  remote_state[0] |= KELVINATOR_POWER;
+  remote_state[8] = remote_state[0];  // Duplicate to the 2nd command chunk.
 }
 
 void IRKelvinatorAC::off() {
-    // state = OFF;
-    remote_state[0] &= ~KELVINATOR_POWER;
-    remote_state[8] = remote_state[0];  // Duplicate to the 2nd command chunk.
+  remote_state[0] &= ~KELVINATOR_POWER;
+  remote_state[8] = remote_state[0];  // Duplicate to the 2nd command chunk.
 }
 
 void IRKelvinatorAC::setPower(bool state) {
@@ -187,20 +191,20 @@ void IRKelvinatorAC::setPower(bool state) {
 }
 
 bool IRKelvinatorAC::getPower() {
-    return ((remote_state[0] & KELVINATOR_POWER) != 0);
+  return ((remote_state[0] & KELVINATOR_POWER) != 0);
 }
 
 // Set the temp. in deg C
 void IRKelvinatorAC::setTemp(uint8_t temp) {
-    temp = std::max((uint8_t) KELVINATOR_MIN_TEMP, temp);
-    temp = std::min((uint8_t) KELVINATOR_MAX_TEMP, temp);
-    remote_state[1] = (remote_state[1] & 0xF0U) | (temp - KELVINATOR_MIN_TEMP);
-    remote_state[9] = remote_state[1];  // Duplicate to the 2nd command chunk.
+  temp = std::max((uint8_t) KELVINATOR_MIN_TEMP, temp);
+  temp = std::min((uint8_t) KELVINATOR_MAX_TEMP, temp);
+  remote_state[1] = (remote_state[1] & 0xF0U) | (temp - KELVINATOR_MIN_TEMP);
+  remote_state[9] = remote_state[1];  // Duplicate to the 2nd command chunk.
 }
 
 // Return the set temp. in deg C
 uint8_t IRKelvinatorAC::getTemp() {
-    return ((remote_state[1] & 0xFU) + KELVINATOR_MIN_TEMP);
+  return ((remote_state[1] & 0xFU) + KELVINATOR_MIN_TEMP);
 }
 
 // Set the speed of the fan, 0-5, 0 is auto, 1-5 is the speed
@@ -222,18 +226,11 @@ void IRKelvinatorAC::setFan(uint8_t fan) {
 }
 
 uint8_t IRKelvinatorAC::getFan() {
-    return ((remote_state[14] & ~KELVINATOR_FAN_MASK) >> KELVINATOR_FAN_OFFSET);
+  return ((remote_state[14] & ~KELVINATOR_FAN_MASK) >> KELVINATOR_FAN_OFFSET);
 }
 
 uint8_t IRKelvinatorAC::getMode() {
-  /*
-  KELVINATOR_AUTO
-  KELVINATOR_COOL
-  KELVINATOR_DRY
-  KELVINATOR_FAN
-  KELVINATOR_HEAT
-  */
-    return (remote_state[0] & ~KELVINATOR_MODE_MASK);
+  return (remote_state[0] & ~KELVINATOR_MODE_MASK);
 }
 
 void IRKelvinatorAC::setMode(uint8_t mode) {
@@ -241,8 +238,9 @@ void IRKelvinatorAC::setMode(uint8_t mode) {
   if (mode > KELVINATOR_HEAT) mode = KELVINATOR_AUTO;
   remote_state[0] = (remote_state[0] & KELVINATOR_MODE_MASK) | mode;
   remote_state[8] = remote_state[0];  // Duplicate to the 2nd command chunk.
-  if (mode == KELVINATOR_AUTO)
-    // When the remote is set to Auto, it defaults to 25C and doesn't show it.
+  if (mode == KELVINATOR_AUTO || KELVINATOR_DRY)
+    // When the remote is set to Auto or Dry, it defaults to 25C and doesn't
+    // show it.
     setTemp(KELVINATOR_AUTO_TEMP);
 }
 
