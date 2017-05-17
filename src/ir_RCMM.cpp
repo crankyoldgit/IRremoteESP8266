e0781f3995b8303a78d515683112b2cabd894279
crankyoldgit <david@xyzzy.com.au>
2017-05-17 16:18:08 +1000

    Unit tests and improvements for the RC-MM protocol. - Unit test coverage for sendRCMM() and decodeRCMM() - Update status for those to Beta as tests now work on them. - Minor code/ordering optimisation. - [Fix] Removed special tolerence for the bit mark which caused issues in   decoding. - Bring RC-MM up to 64bit support. - Add repeating to sendRCMM.
    This should address issue #21 when v2.0 is released.

--- src/ir_RCMM.cpp
+++ src/ir_RCMM.cpp
@@ -31,58 +31,83 @@
 
 #if SEND_RCMM
 // Send a Philips RC-MM packet.
-// Based on http://www.sbprojects.com/knowledge/ir/rcmm.php
+//
 // Args:
 //   data: The data we want to send. MSB first.
 //   nbits: The number of bits of data to send. (Typically 12, 24, or 32[Nokia])
+//   repeat: The nr. of times the message should be sent.
 //
-// Status:  ALPHA (untested and unconfirmed.)
-void IRsend::sendRCMM(uint32_t data, uint8_t nbits) {
+// Status:  BETA / Should be working.
+//
+// Ref:
+//   http://www.sbprojects.com/knowledge/ir/rcmm.php
+void IRsend::sendRCMM(uint64_t data, uint16_t nbits, uint16_t repeat) {
   // Set 36kHz IR carrier frequency & a 1/3 (33%) duty cycle.
   enableIROut(36, 33);
   IRtimer usecs = IRtimer();
 
-  // Header
-  mark(RCMM_HDR_MARK);
-  space(RCMM_HDR_SPACE);
-  // Data
-  uint32_t mask = 0b11 << (nbits - 2);
-  // RC-MM sends data 2 bits at a time.
-  for (uint8_t i = nbits; i > 0; i -= 2) {
-    mark(RCMM_BIT_MARK);
-    // Grab the next Most Significant Bits to send.
-    switch ((data & mask) >> (i - 2)) {
-      case 0b00: space(RCMM_BIT_SPACE_0); break;
-      case 0b01: space(RCMM_BIT_SPACE_1); break;
-      case 0b10: space(RCMM_BIT_SPACE_2); break;
-      case 0b11: space(RCMM_BIT_SPACE_3); break;
+  for (uint16_t r = 0; r <= repeat; r++) {
+    usecs.reset();
+    // Header
+    mark(RCMM_HDR_MARK);
+    space(RCMM_HDR_SPACE);
+    // Data
+    uint64_t mask = 0b11ULL << (nbits - 2);
+    // RC-MM sends data 2 bits at a time.
+    for (int32_t i = nbits; i > 0; i -= 2) {
+      mark(RCMM_BIT_MARK);
+      // Grab the next Most Significant Bits to send.
+      switch ((data & mask) >> (i - 2)) {
+        case 0b00: space(RCMM_BIT_SPACE_0); break;
+        case 0b01: space(RCMM_BIT_SPACE_1); break;
+        case 0b10: space(RCMM_BIT_SPACE_2); break;
+        case 0b11: space(RCMM_BIT_SPACE_3); break;
+      }
+      mask >>= 2;
     }
-    mask >>= 2;
+    // Footer
+    mark(RCMM_BIT_MARK);
+    // Protocol requires us to wait at least RCMM_RPT_LENGTH usecs from the
+    // start or RCMM_MIN_GAP usecs.
+    space(std::max(RCMM_RPT_LENGTH - usecs.elapsed(), RCMM_MIN_GAP));
   }
-  // Footer
-  mark(RCMM_BIT_MARK);
-  // Protocol requires us to wait at least RCMM_RPT_LENGTH usecs from the start
-  // or RCMM_MIN_GAP usecs.
-  space(std::max(RCMM_RPT_LENGTH - usecs.elapsed(), RCMM_MIN_GAP));
 }
 #endif
 
 #if DECODE_RCMM
 // Decode a Philips RC-MM packet (between 12 & 32 bits) if possible.
 // Places successful decode information in the results pointer.
+// Args:
+//   results: Ptr to the data to decode and where to store the decode result.
+//   nbits:   Nr. of bits to expect in the data portion. Typically RCMM_BITS.
+//   strict:  Flag to indicate if we strictly adhere to the specification.
 // Returns:
-//   The decode success status.
+//   boolean: True if it can decode it, false if it can't.
 //
-// Status:  ALPHA (untested and unconfirmed.)
+// Status:  BETA / Should be working.
+//
 // Ref:
 //   http://www.sbprojects.com/knowledge/ir/rcmm.php
-bool IRrecv::decodeRCMM(decode_results *results) {
-  uint32_t data = 0;
+bool IRrecv::decodeRCMM(decode_results *results, uint16_t nbits, bool strict) {
+  uint64_t data = 0;
   uint16_t offset = OFFSET_START;
 
-  int16_t bitSize = results->rawlen - 4;
-  if (bitSize < 12 || bitSize > 32)
-    return false;
+  if (results->rawlen <= 4)
+    return false;  // Not enough entries to ever be RCMM.
+
+  // Calc the maximum size in bits, the message can be, or that we can accept.
+  int16_t maxBitSize = std::min((uint16_t) results->rawlen - 4,
+                                (uint16_t) sizeof(data) * 8);
+  // Compliance
+  if (strict) {
+    // Technically the spec says bit sizes should be 12 xor 24. however
+    // 32 bits has been seen from a device. We are going to assume
+    // 12 <= bits <= 32 is the 'required' bit length for the spec.
+    if (maxBitSize < 12 || maxBitSize > 32)
+      return false;
+    if (maxBitSize < nbits)
+      return false;  // Short cut, we can never reach the expected nr. of bits.
+  }
   // Header decode
   if (!matchMark(results->rawbuf[offset++], RCMM_HDR_MARK))
     return false;
@@ -90,12 +115,14 @@ bool IRrecv::decodeRCMM(decode_results *results) {
     return false;
   // Data decode
   // RC-MM has two bits of data per mark/space pair.
-  for (uint16_t i = 0; i < bitSize; i += 2, offset++) {
+  uint16_t actualBits;
+  for (actualBits = 0; actualBits < maxBitSize; actualBits += 2, offset++) {
+    if (!matchMark(results->rawbuf[offset++], RCMM_BIT_MARK))
+      return false;
+
     data <<= 2;
     // Use non-default tolerance & excess for matching some of the spaces as the
     // defaults are too generous and causes mis-matches in some cases.
-    if (!matchMark(results->rawbuf[offset++], RCMM_BIT_MARK, RCMM_TOLERANCE))
-      return false;
     if (matchSpace(results->rawbuf[offset],
                    RCMM_BIT_SPACE_0, TOLERANCE, RCMM_EXCESS))
       data += 0;
@@ -115,10 +142,14 @@ bool IRrecv::decodeRCMM(decode_results *results) {
   if (!matchMark(results->rawbuf[offset], RCMM_BIT_MARK))
     return false;
 
+  // Compliance
+  if (strict && actualBits != nbits)
+    return false;
+
   // Success
   results->value = data;
   results->decode_type = RCMM;
-  results->bits = bitSize;
+  results->bits = actualBits;
   results->address = 0;
   results->command = 0;
   return true;
