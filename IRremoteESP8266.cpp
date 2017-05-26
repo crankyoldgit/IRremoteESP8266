25d97adff74633d1a313c03e51951a04bcb6a176
David Conran <crankyoldgit@users.noreply.github.com>
2017-05-26 20:29:05 +1000

    Add a better post-message gap.

--- IRremoteESP8266.cpp
+++ IRremoteESP8266.cpp
@@ -486,9 +486,7 @@ void ICACHE_FLASH_ATTR IRsend::sendGree(unsigned char data[]) {
 
   // Footer #2
   mark(GREE_BIT_MARK);
-  space(0);
-
-  ledOff();
+  space(GREE_MSG_SPACE);
 }
 
 void ICACHE_FLASH_ATTR IRsend::mark(unsigned int usec) {
