325d284a2693b89a53ca7a07a4e0fc3670c1f6ec
crankyoldgit <david@xyzzy.com.au>
2017-05-26 16:13:41 +1000

    Unit tests for the Daikin A/C protocol and v2.0 upgrade. - Add unit tests for sendDaikin() and the IRDaikinESP class. - Add additional methods to the IRDaikinESP class to bring it inline with   similar A/C classes. - Add some useful defines/constants for users and code style. - Re-work how mode and temp are set. - Support sending longer byte lengths, and repeats to sendDaikin().   i.e. v2.0 spec. - Remove a C+11-ism compiler warning. (setting an array when declaring it.)

--- src/ir_Daikin.cpp
+++ src/ir_Daikin.cpp
@@ -6,6 +6,7 @@ http://harizanov.com/2012/02/control-daikin-air-conditioner-over-the-internet/
 Copyright 2016 sillyfrog
 */
 
+#include <algorithm>
 #include "ir_Daikin.h"
 #include "IRutils.h"
 
@@ -37,33 +38,39 @@ Copyright 2016 sillyfrog
 // Ref:
 //   IRDaikinESP.cpp
 //   https://github.com/mharizanov/Daikin-AC-remote-control-over-the-Internet/tree/master/IRremote
-void IRsend::sendDaikin(unsigned char data[]) {
+void IRsend::sendDaikin(unsigned char data[], uint16_t nbytes,
+                        uint16_t repeat) {
+  if (nbytes < DAIKIN_COMMAND_LENGTH)
+    return;  // Not enough bytes to send a proper message.
   // Set IR carrier frequency
   enableIROut(38);
-  // Header #1
-  mark(DAIKIN_HDR_MARK);
-  space(DAIKIN_HDR_SPACE);
-  // Data #1
-  for (uint8_t i = 0; i < 8; i++)
-    sendData(DAIKIN_ONE_MARK, DAIKIN_ONE_SPACE, DAIKIN_ZERO_MARK,
-             DAIKIN_ZERO_SPACE, data[i], 8, false);
-  // Footer #1
-  mark(DAIKIN_ONE_MARK);
-  space(DAIKIN_ZERO_SPACE + 29000);
+  for (uint16_t r = 0; r <= repeat; r++) {
+    // Header #1
+    mark(DAIKIN_HDR_MARK);
+    space(DAIKIN_HDR_SPACE);
+    // Data #1
+    for (uint16_t i = 0; i < 8 && i < nbytes; i++)
+      sendData(DAIKIN_ONE_MARK, DAIKIN_ONE_SPACE, DAIKIN_ZERO_MARK,
+               DAIKIN_ZERO_SPACE, data[i], 8, false);
+    // Footer #1
+    mark(DAIKIN_ONE_MARK);
+    space(DAIKIN_ZERO_SPACE + DAIKIN_GAP);
 
-  // Header #2
-  mark(DAIKIN_HDR_MARK);
-  space(DAIKIN_HDR_SPACE);
-  // Data #2
-  for (uint8_t i = 8; i < DAIKIN_COMMAND_LENGTH; i++)
-    sendData(DAIKIN_ONE_MARK, DAIKIN_ONE_SPACE, DAIKIN_ZERO_MARK,
-             DAIKIN_ZERO_SPACE, data[i], 8, false);
-  // Footer #2
-  mark(DAIKIN_ONE_MARK);
-  space(DAIKIN_ZERO_SPACE);
+    // Header #2
+    mark(DAIKIN_HDR_MARK);
+    space(DAIKIN_HDR_SPACE);
+    // Data #2
+    for (uint16_t i = 8; i < nbytes; i++)
+      sendData(DAIKIN_ONE_MARK, DAIKIN_ONE_SPACE, DAIKIN_ZERO_MARK,
+               DAIKIN_ZERO_SPACE, data[i], 8, false);
+    // Footer #2
+    mark(DAIKIN_ONE_MARK);
+    space(DAIKIN_ZERO_SPACE + DAIKIN_GAP);
+  }
 }
 
 IRDaikinESP::IRDaikinESP(uint16_t pin) : _irsend(pin) {
+  stateReset();
 }
 
 void IRDaikinESP::begin() {
@@ -88,6 +95,32 @@ void IRDaikinESP::checksum() {
   daikin[26] = sum & 0xFF;
 }
 
+
+void IRDaikinESP::stateReset() {
+  for (uint8_t i = 4; i < DAIKIN_COMMAND_LENGTH; i++)
+    daikin[i] = 0x0;
+
+  daikin[0] = 0x11;
+  daikin[1] = 0xDA;
+  daikin[2] = 0x27;
+  daikin[3] = 0xF0;
+  daikin[7] = 0x20;
+  daikin[8] = 0x11;
+  daikin[9] = 0xDA;
+  daikin[10] = 0x27;
+  daikin[13] = 0x41;
+  daikin[14] = 0x1E;
+  daikin[16] = 0xB0;
+  daikin[23] = 0xC0;
+  daikin[26] = 0xE3;
+  checksum();
+}
+
+uint8_t* IRDaikinESP::getRaw() {
+  checksum();   // Ensure correct settings before sending.
+  return daikin;
+}
+
 void IRDaikinESP::on() {
   // state = ON;
   daikin[13] |= 0x01;
@@ -98,6 +131,13 @@ void IRDaikinESP::off() {
   // state = OFF;
   daikin[13] &= 0xFE;
   checksum();
+}
+
+void IRDaikinESP::setPower(bool state) {
+  if (state)
+    on();
+  else
+    off();
 }
 
 uint8_t IRDaikinESP::getPower() {
@@ -114,13 +154,34 @@ uint8_t IRDaikinESP::getAux() {
   return daikin[21];
 }
 
+void IRDaikinESP::setQuiet(bool state) {
+  if (state)
+    setAux(DAIKIN_SILENT);
+  else
+    setAux(0x0);
+}
+
+bool IRDaikinESP::getQuiet() {
+  return (getAux() == DAIKIN_SILENT);
+}
+
+void IRDaikinESP::setPowerful(bool state) {
+  if (state)
+    setAux(DAIKIN_POWERFUL);
+  else
+    setAux(0x0);
+}
+
+bool IRDaikinESP::getPowerful() {
+  return (getAux() == DAIKIN_POWERFUL);
+}
 
 // Set the temp in deg C
 void IRDaikinESP::setTemp(uint8_t temp) {
-  if (temp < 18)
-    temp = 18;
-  else if (temp > 32)
-    temp = 32;
+  if (temp < DAIKIN_MIN_TEMP)
+    temp = DAIKIN_MIN_TEMP;
+  else if (temp > DAIKIN_MAX_TEMP)
+    temp = DAIKIN_MAX_TEMP;
   daikin[14] = temp * 2;
   checksum();
 }
@@ -134,10 +195,11 @@ void IRDaikinESP::setFan(uint8_t fan) {
   // Set the fan speed bits, leave low 4 bits alone
   uint8_t fanset;
   daikin[16] &= 0x0F;
-  if (fan >= 1 && fan <= 5)
-    fanset = 0x20 + (0x10 * fan);
-  else
+  fan = std::min(fan, DAIKIN_FAN_MAX);
+  if (fan == DAIKIN_FAN_AUTO)
     fanset = 0xA0;
+  else
+    fanset = 0x20 + (0x10 * fan);
   daikin[16] |= fanset;
   checksum();
 }
@@ -145,8 +207,8 @@ void IRDaikinESP::setFan(uint8_t fan) {
 uint8_t IRDaikinESP::getFan() {
   uint8_t fan = daikin[16] >> 4;
   fan -= 2;
-  if (fan > 5)
-    fan = 0;
+  if (fan > DAIKIN_FAN_MAX)
+    fan = DAIKIN_FAN_AUTO;
   return fan;
 }
 
@@ -162,34 +224,43 @@ uint8_t IRDaikinESP::getMode() {
 }
 
 void IRDaikinESP::setMode(uint8_t mode) {
+  switch (mode) {
+    case DAIKIN_COOL:
+    case DAIKIN_HEAT:
+    case DAIKIN_FAN:
+    case DAIKIN_DRY:
+      break;
+    default:
+      mode = DAIKIN_AUTO;
+  }
   daikin[13] = (mode << 4) | getPower();
   checksum();
 }
 
-void IRDaikinESP::setSwingVertical(uint8_t swing) {
-  if (swing)
+void IRDaikinESP::setSwingVertical(bool state) {
+  if (state)
     daikin[16] |= 0x0F;
   else
     daikin[16] &= 0xF0;
   checksum();
 }
 
-uint8_t IRDaikinESP::getSwingVertical() {
+bool IRDaikinESP::getSwingVertical() {
   return daikin[16] & 0x01;
 }
 
-void IRDaikinESP::setSwingHorizontal(uint8_t swing) {
-  if (swing)
+void IRDaikinESP::setSwingHorizontal(bool state) {
+  if (state)
     daikin[17] |= 0x0F;
   else
     daikin[17] &= 0xF0;
   checksum();
 }
 
-uint8_t IRDaikinESP::getSwingHorizontal() {
+bool IRDaikinESP::getSwingHorizontal() {
   return daikin[17] & 0x01;
 }
-#endif
+#endif  // SEND_DAIKIN
 
 #if DECODE_DAIKIN
 // TODO(crankyoldgit): NOT WORKING. This needs to be finished.
@@ -277,4 +348,4 @@ bool IRrecv::decodeDaikin(decode_results *results, uint16_t nbits,
   results->command = 0;
   return true;
 }
-#endif
+#endif  // DECODE_DAIKIN
