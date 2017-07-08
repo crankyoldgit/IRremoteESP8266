7f0fea89f7a15231385e2766e0795bf42f70e544
crankyoldgit <david@xyzzy.com.au>
2017-07-08 12:04:30 +1000

    Update decodeWhynter() to use auto-calibration.

--- src/ir_Whynter.cpp
+++ src/ir_Whynter.cpp
@@ -5,6 +5,7 @@
 #include "IRrecv.h"
 #include "IRsend.h"
 #include "IRtimer.h"
+#include "IRutils.h"
 
 //               W   W  H   H  Y   Y N   N TTTTT EEEEE  RRRRR
 //               W   W  H   H   Y Y  NN  N   T   E      R   R
@@ -16,15 +17,27 @@
 // Whynter originally added from https://github.com/shirriff/Arduino-IRremote/
 
 // Constants
-#define WHYNTER_HDR_MARK             2850U
-#define WHYNTER_HDR_SPACE            2850U
-#define WHYNTER_BIT_MARK              750U
-#define WHYNTER_ONE_SPACE            2150U
-#define WHYNTER_ZERO_SPACE            750U
-#define WHYNTER_MIN_COMMAND_LENGTH 108000UL  // Completely made up value.
-#define WHYNTER_MIN_GAP WHYNTER_MIN_COMMAND_LENGTH - \
-    (2 * (WHYNTER_BIT_MARK + WHYNTER_ZERO_SPACE) + \
-     WHYNTER_BITS * (WHYNTER_BIT_MARK + WHYNTER_ONE_SPACE))
+#define WHYNTER_TICK                       50U
+#define WHYNTER_HDR_MARK_TICKS             57U
+#define WHYNTER_HDR_MARK                 (WHYNTER_HDR_MARK_TICKS * WHYNTER_TICK)
+#define WHYNTER_HDR_SPACE_TICKS            57U
+#define WHYNTER_HDR_SPACE                (WHYNTER_HDR_SPACE_TICKS * \
+                                          WHYNTER_TICK)
+#define WHYNTER_BIT_MARK_TICKS             15U
+#define WHYNTER_BIT_MARK                 (WHYNTER_BIT_MARK_TICKS * WHYNTER_TICK)
+#define WHYNTER_ONE_SPACE_TICKS            43U
+#define WHYNTER_ONE_SPACE                (WHYNTER_ONE_SPACE_TICKS * \
+                                          WHYNTER_TICK)
+#define WHYNTER_ZERO_SPACE_TICKS           15U
+#define WHYNTER_ZERO_SPACE               (WHYNTER_ZERO_SPACE_TICKS * \
+                                          WHYNTER_TICK)
+#define WHYNTER_MIN_COMMAND_LENGTH_TICKS 2160U  // Completely made up value.
+#define WHYNTER_MIN_COMMAND_LENGTH       (WHYNTER_MIN_COMMAND_LENGTH_TICKS * \
+                                          WHYNTER_TICK)
+#define WHYNTER_MIN_GAP_TICKS            (WHYNTER_MIN_COMMAND_LENGTH_TICKS - \
+    (2 * (WHYNTER_BIT_MARK_TICKS + WHYNTER_ZERO_SPACE_TICKS) + \
+     WHYNTER_BITS * (WHYNTER_BIT_MARK_TICKS + WHYNTER_ONE_SPACE_TICKS)))
+#define WHYNTER_MIN_GAP                  (WHYNTER_MIN_GAP_TICKS * WHYNTER_TICK)
 
 #if SEND_WHYNTER
 // Send a Whynter message.
@@ -87,35 +100,37 @@ bool IRrecv::decodeWhynter(decode_results *results, uint16_t nbits,
   uint16_t offset = OFFSET_START;
 
   // Header
-  // Sequence begins with a bit mark and a zero space
-  if (!matchMark(results->rawbuf[offset++], WHYNTER_BIT_MARK))
-    return false;
-  if (!matchSpace(results->rawbuf[offset++], WHYNTER_ZERO_SPACE))
-    return false;
-  // header mark and space
-  if (!matchMark(results->rawbuf[offset++], WHYNTER_HDR_MARK))
-    return false;
-  if (!matchSpace(results->rawbuf[offset++], WHYNTER_HDR_SPACE))
-    return false;
+  // Sequence begins with a bit mark and a zero space.
+  // These are typically small, so we'll prefer to do the calibration
+  // on the much larger header mark & space that are next.
+  if (!matchMark(results->rawbuf[offset++], WHYNTER_BIT_MARK)) return false;
+  if (!matchSpace(results->rawbuf[offset++], WHYNTER_ZERO_SPACE)) return false;
+  // Main header mark and space
+  if (!matchMark(results->rawbuf[offset], WHYNTER_HDR_MARK)) return false;
+  // Calculate how long the common tick time is based on the header mark.
+  uint32_t m_tick = calcTickTime(results->rawbuf[offset++],
+                                 WHYNTER_HDR_MARK_TICKS);
+  if (!matchSpace(results->rawbuf[offset], WHYNTER_HDR_SPACE)) return false;
+  // Calculate how long the common tick time is based on the header space.
+  uint32_t s_tick = calcTickTime(results->rawbuf[offset++],
+                                 WHYNTER_HDR_SPACE_TICKS);
 
   // Data
   uint64_t data = 0;
-  for (uint16_t i = 0; i < nbits; i++, offset++) {
-    if (!matchMark(results->rawbuf[offset++], WHYNTER_BIT_MARK))
-      return false;
-    if (matchSpace(results->rawbuf[offset], WHYNTER_ONE_SPACE))
-      data = (data << 1) | 1;  // 1
-    else if (matchSpace(results->rawbuf[offset], WHYNTER_ZERO_SPACE))
-      data <<= 1;  // 0
-    else
-      return false;
-  }
+  match_result_t data_result = matchData(&(results->rawbuf[offset]), nbits,
+                                         WHYNTER_BIT_MARK_TICKS * m_tick,
+                                         WHYNTER_ONE_SPACE_TICKS * s_tick,
+                                         WHYNTER_BIT_MARK_TICKS * m_tick,
+                                         WHYNTER_ZERO_SPACE_TICKS * s_tick);
+  if (data_result.success == false) return false;
+  data = data_result.data;
+  offset += data_result.used;
 
   // Footer
-  if (!matchMark(results->rawbuf[offset++], WHYNTER_BIT_MARK))
+  if (!matchMark(results->rawbuf[offset++], WHYNTER_BIT_MARK_TICKS * m_tick))
     return false;
   if (offset < results->rawlen &&
-      !matchAtLeast(results->rawbuf[offset], WHYNTER_MIN_GAP))
+      !matchAtLeast(results->rawbuf[offset], WHYNTER_MIN_GAP_TICKS * s_tick))
     return false;
 
   // Success
