625a50bafa7db5908c0eb230879d06d1b011e7eb
David Conran <crankyoldgit@users.noreply.github.com>
2017-07-10 10:00:54 +1000

    Add an example of sendRaw() to IRsendDemo.ino (#270)
    People seem to ask how to do this fairly regularly (#269).
    Adding an example of it.

--- examples/IRsendDemo/IRsendDemo.ino
+++ examples/IRsendDemo/IRsendDemo.ino
@@ -35,6 +35,15 @@
 
 IRsend irsend(4);  // An IR LED is controlled by GPIO pin 4 (D2)
 
+// Example of data captured by IRrecvDumpV2.ino
+uint16_t rawData[67] = {9000, 4500, 650, 550, 650, 1650, 600, 550, 650, 550,
+                        600, 1650, 650, 550, 600, 1650, 650, 1650, 650, 1650,
+                        600, 550, 650, 1650, 650, 1650, 650, 550, 600, 1650,
+                        650, 1650, 650, 550, 650, 550, 650, 1650, 650, 550,
+                        650, 550, 650, 550, 600, 550, 650, 550, 650, 550,
+                        650, 1650, 600, 550, 650, 1650, 650, 1650, 650, 1650,
+                        650, 1650, 650, 1650, 650, 1650, 600};
+
 void setup() {
   irsend.begin();
   Serial.begin(115200, SERIAL_8N1, SERIAL_TX_ONLY);
@@ -46,5 +55,8 @@ void loop() {
   delay(2000);
   Serial.println("Sony");
   irsend.sendSony(0xa90, 12, 2);
+  delay(2000);
+  Serial.println("a rawData capture from IRrecvDumpV2");
+  irsend.sendRaw(rawData, 67, 38);  // Send a raw data capture at 38kHz.
   delay(2000);
 }
