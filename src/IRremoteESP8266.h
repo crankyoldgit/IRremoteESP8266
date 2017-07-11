03b500c5d861b7ad68a4910a6fae0bcfeb9ab9bc
crankyoldgit <david@xyzzy.com.au>
2017-07-11 12:40:00 +1000

    Update version to 2.1.1

--- src/IRremoteESP8266.h
+++ src/IRremoteESP8266.h
@@ -45,7 +45,7 @@
 #endif
 
 // Library Version
-#define _IRREMOTEESP8266_VERSION_ "2.1.0"
+#define _IRREMOTEESP8266_VERSION_ "2.1.1"
 // Supported IR protocols
 // Each protocol you include costs memory and, during decode, costs time
 // Disable (set to false) all the protocols you do not need/want!
