8d18852cff1480b3f5ae1e87c7423d8348b88128
jorgecis <jorgecis@gmail.com>
2017-09-30 01:02:11 -0500

    Program can get a GC argument a decode it

--- tool/main.cpp
+++ tool/main.cpp
@@ -1,26 +1,58 @@
 #include "IRsend.h"
 #include "IRsend_test.h"
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <inttypes.h> /* strtoumax */
+#include <stdbool.h>
+
+
 using namespace std;
 
-int main (void) {
+
+void str_to_uint16(char *str, uint16_t *res)
+{
+  char *end;
+  errno = 0;
+  intmax_t val = strtoimax(str, &end, 10);
+  if (errno == ERANGE || val < 0 || val > UINT16_MAX || end == str || *end != '\0')
+    return;
+  *res = (uint16_t) val;
+}
+
+
+int main (int argc, char * argv[]) {
+
+
+    if (argc != 2) { 
+        cout << "Use main [global_code]" << endl;
+	return 0;
+    }
+
+    uint16_t gc_test[150];
+    int index = 0;
+
+    char * pch;
+    pch = strtok (argv[1],",");
+    while (pch != NULL)
+    {
+        str_to_uint16(pch, &gc_test[index]);
+        pch = strtok (NULL, " ,");
+	index++;
+    }
 
     IRsendTest irsend(4);
     IRrecv irrecv(4);
     irsend.begin();
     irsend.reset();
 
-    uint16_t gc_test[71] = {38000, 1, 1, 342, 172, 21, 22, 21, 21, 21, 65, 21, 21,
-                          21, 22, 21, 22, 21, 21, 21, 22, 21, 65, 21, 65, 21,
-                          22, 21, 65, 21, 65, 21, 65, 21, 65, 21, 65, 21, 65,
-                          21, 22, 21, 22, 21, 21, 21, 22, 21, 22, 21, 65, 21,
-                          22, 21, 21, 21, 65, 21, 65, 21, 65, 21, 64, 22, 65,
-                          21, 22, 21, 65, 21, 1519};
-    irsend.sendGC(gc_test, 71);
+
+    irsend.sendGC(gc_test, index);
     irsend.makeDecodeResult();
+    irrecv.decode(&irsend.capture);
 
-    irrecv.decodeNEC(&irsend.capture);
-
+    cout << "length " << index << endl;
     cout << "Code type " << irsend.capture.decode_type << endl;
     cout << "Code bits " << irsend.capture.bits << endl;
     cout << "Code value " << std::hex << irsend.capture.value << endl;
