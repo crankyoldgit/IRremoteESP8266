d6d83d41ccdb6d600cda710af8e70657946fbab7
David Conran <david@xyzzy.com.au>
2017-10-03 21:30:03 +1000

    Initial support for Nikai TV devices. - See Issue #309 for more details and background.

--- src/IRremoteESP8266.h
+++ src/IRremoteESP8266.h
@@ -132,6 +132,9 @@
 #define DECODE_TROTEC        false  // Not implemented.
 #define SEND_TROTEC          true
 
+#define DECODE_NIKAI     true
+#define SEND_NIKAI       true
+
 /*
  * Always add to the end of the list and should never remove entries
  * or change order. Projects may save the type number for later usage
@@ -167,7 +170,8 @@ enum decode_type_t {
   PRONTO,
   NEC_LIKE,
   ARGO,
-  TROTEC
+  TROTEC,
+  NIKAI
 };
 
 // Message lengths & required repeat values
@@ -222,6 +226,7 @@ enum decode_type_t {
 #define TROTEC_COMMAND_LENGTH        9U
 #define WHYNTER_BITS                32U
 #define ARGO_COMMAND_LENGTH         12U
+#define NIKAI_BITS                  24U
 
 // Turn on Debugging information by uncommenting the following line.
 // #define DEBUG 1
