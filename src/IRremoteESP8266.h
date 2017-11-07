38579d9ee1fdd907d37efea2b9f3a802d472e868
crankyoldgit <david@xyzzy.com.au>
2017-11-08 09:40:27 +1000

    Dev branch for the 2.3.0 release.

--- src/IRremoteESP8266.h
+++ src/IRremoteESP8266.h
@@ -46,7 +46,7 @@
 #endif
 
 // Library Version
-#define _IRREMOTEESP8266_VERSION_ "2.2.1"
+#define _IRREMOTEESP8266_VERSION_ "2.3.0"
 // Supported IR protocols
 // Each protocol you include costs memory and, during decode, costs time
 // Disable (set to false) all the protocols you do not need/want!
