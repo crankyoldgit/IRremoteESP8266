c8eb31de4d0b9f2bc4e5a69ff5bb46f9b00e9ee7
David Conran <crankyoldgit@users.noreply.github.com>
2017-11-10 14:34:20 +0800

    Initial support for sending Toshiba A/C messages. (#333)
    * Initial support for sending Toshiba A/C messages.
    * Add known-working A/C unit model info.

--- src/IRremoteESP8266.h
+++ src/IRremoteESP8266.h
@@ -21,7 +21,7 @@
  * Kelvinator A/C and Sherwood added by crankyoldgit
  * Mitsubishi (TV) sending added by crankyoldgit
  * Pronto code sending added by crankyoldgit
- * Mitsubishi A/C added by crankyoldgit
+ * Mitsubishi & Toshiba A/C added by crankyoldgit
  *     (derived from https://github.com/r45635/HVAC-IR-Control)
  * DISH decode by marcosamarinho
  * Gree Heatpump sending added by Ville Skyttä (scop)
@@ -136,8 +136,11 @@
 #define DECODE_NIKAI         true
 #define SEND_NIKAI           true
 
+#define DECODE_TOSHIBA_AC    false  // Not implemented.
+#define SEND_TOSHIBA_AC      true
+
 #if (DECODE_ARGO || DECODE_DAIKIN || DECODE_GREE || DECODE_KELVINATOR || \
-     DECODE_MITSUBISHI_AC || DECODE_TROTEC)
+     DECODE_MITSUBISHI_AC || DECODE_TOSHIBA_AC || DECODE_TROTEC)
 #define DECODE_AC true  // We need some common infrastructure for decoding A/Cs.
 #else
 #define DECODE_AC false   // We don't need that infrastructure.
@@ -180,7 +183,8 @@ enum decode_type_t {
   TROTEC,
   NIKAI,
   RAW,  // Technically not a protocol, but an encoding.
-  GLOBALCACHE  // Technically not a protocol, but an encoding.
+  GLOBALCACHE,  // Technically not a protocol, but an encoding.
+  TOSHIBA_AC
 };
 
 // Message lengths & required repeat values
@@ -233,6 +237,8 @@ enum decode_type_t {
 #define SONY_20_BITS                20U
 #define SONY_MIN_BITS      SONY_12_BITS
 #define SONY_MIN_REPEAT              2U
+#define TOSHIBA_AC_STATE_LENGTH      9U
+#define TOSHIBA_AC_MIN_REPEAT        0U
 #define TROTEC_COMMAND_LENGTH        9U
 #define WHYNTER_BITS                32U
 #define ARGO_COMMAND_LENGTH         12U
