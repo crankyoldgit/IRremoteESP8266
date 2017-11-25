01b9f766a7c27b8dd199219a0cb59004160418c4
crankyoldgit <david@xyzzy.com.au>
2017-11-26 00:24:54 +1000

    Better output for dumpV2 example code. * Display a human-readable version of some A/C messages. * Move some common code into functions. * Streamline some of the serial output into single calls. * Display the library version number in capture details.

--- src/IRutils.h
+++ src/IRutils.h
@@ -11,13 +11,19 @@
 #ifndef ARDUINO
 #include <string>
 #endif
+#include "IRremoteESP8266.h"
 
 uint64_t reverseBits(uint64_t input, uint16_t nbits);
 #ifdef ARDUINO  // Arduino's & C++'s string implementations can't co-exist.
 String uint64ToString(uint64_t input, uint8_t base = 10);
+String typeToString(const decode_type_t protocol,
+                    const bool isRepeat = false);
+void serialPrintUint64(uint64_t input, uint8_t base = 10);
 #else
 std::string uint64ToString(uint64_t input, uint8_t base = 10);
+std::string typeToString(const decode_type_t protocol,
+                         const bool isRepeat = false);
 #endif
-void serialPrintUint64(uint64_t input, uint8_t base = 10);
+bool hasACState(const decode_type_t protocol);
 
 #endif  // IRUTILS_H_
