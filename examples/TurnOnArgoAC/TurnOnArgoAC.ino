1f9d0abc3d5494b05d9aaa29439634de1a2dc925
David Conran <crankyoldgit@users.noreply.github.com>
2018-03-02 12:54:37 +0000

    Use better comments for send GPIO in examples. (#425)
    Per Issue #420

--- examples/TurnOnArgoAC/TurnOnArgoAC.ino
+++ examples/TurnOnArgoAC/TurnOnArgoAC.ino
@@ -1,5 +1,6 @@
 /* Copyright 2017 crankyoldgit
-* An IR LED circuit *MUST* be connected to ESP8266 GPIO4.
+* An IR LED circuit *MUST* be connected to the ESP8266 on a pin
+* as specified by IR_LED below.
 *
 * TL;DR: The IR LED needs to be driven by a transistor for a good result.
 *
@@ -29,7 +30,8 @@
 #include <IRsend.h>
 #include <ir_Argo.h>
 
-IRArgoAC argoir(4);  // An IR LED is controlled by GPIO4, NodeMCU D2
+#define IR_LED 4  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
+IRArgoAC argoir(IR_LED);  // Set the GPIO to be used to sending the message.
 
 void setup() {
   argoir.begin();
