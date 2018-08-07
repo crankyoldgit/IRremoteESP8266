aa154c962c37ba4b728508cc60073b1b9669104d
crankyoldgit <david@xyzzy.com.au>
2018-08-07 15:47:34 +1000

    Convert JVC #define's to const's.

--- src/ir_JVC.cpp
+++ src/ir_JVC.cpp
@@ -19,24 +19,24 @@
 // Constants
 // Ref:
 //   http://www.sbprojects.com/knowledge/ir/jvc.php
-#define JVC_TICK              75U
-#define JVC_HDR_MARK_TICKS   112U
-#define JVC_HDR_MARK         (JVC_HDR_MARK_TICKS * JVC_TICK)
-#define JVC_HDR_SPACE_TICKS   56U
-#define JVC_HDR_SPACE        (JVC_HDR_SPACE_TICKS * JVC_TICK)
-#define JVC_BIT_MARK_TICKS     7U
-#define JVC_BIT_MARK         (JVC_BIT_MARK_TICKS * JVC_TICK)
-#define JVC_ONE_SPACE_TICKS   23U
-#define JVC_ONE_SPACE        (JVC_ONE_SPACE_TICKS * JVC_TICK)
-#define JVC_ZERO_SPACE_TICKS  7U
-#define JVC_ZERO_SPACE       (JVC_ZERO_SPACE_TICKS * JVC_TICK)
-#define JVC_RPT_LENGTH_TICKS 800U
-#define JVC_RPT_LENGTH       (JVC_RPT_LENGTH_TICKS * JVC_TICK)
-#define JVC_MIN_GAP_TICKS    (JVC_RPT_LENGTH_TICKS - \
-    (JVC_HDR_MARK_TICKS + JVC_HDR_SPACE_TICKS + \
-     kJVCBits * (JVC_BIT_MARK_TICKS + JVC_ONE_SPACE_TICKS) + \
-     JVC_BIT_MARK_TICKS))
-#define JVC_MIN_GAP          (JVC_MIN_GAP_TICKS * JVC_TICK)
+const uint16_t kJVCTick = 75;
+const uint16_t kJVCHdrMarkTicks = 112;
+const uint16_t kJVCHdrMark = kJVCHdrMarkTicks * kJVCTick;
+const uint16_t kJVCHdrSpaceTicks = 56;
+const uint16_t kJVCHdrSpace = kJVCHdrSpaceTicks * kJVCTick;
+const uint16_t kJVCBitMarkTicks = 7;
+const uint16_t kJVCBitMark = kJVCBitMarkTicks * kJVCTick;
+const uint16_t kJVCOneSpaceTicks = 23;
+const uint16_t kJVCOneSpace = kJVCOneSpaceTicks * kJVCTick;
+const uint16_t kJVCZeroSpaceTicks = 7;
+const uint16_t kJVCZeroSpace = kJVCZeroSpaceTicks * kJVCTick;
+const uint16_t kJVCRptLengthTicks = 800;
+const uint16_t kJVCRptLength = kJVCRptLengthTicks * kJVCTick;
+const uint16_t kJVCMinGapTicks = kJVCRptLengthTicks -
+    (kJVCHdrMarkTicks + kJVCHdrSpaceTicks +
+     kJVCBits * (kJVCBitMarkTicks + kJVCOneSpaceTicks) +
+     kJVCBitMarkTicks);
+const uint16_t kJVCMinGap = kJVCMinGapTicks * kJVCTick;
 
 #if SEND_JVC
 // Send a JVC message.
@@ -57,23 +57,23 @@ void IRsend::sendJVC(uint64_t data, uint16_t nbits, uint16_t repeat) {
   IRtimer usecs = IRtimer();
   // Header
   // Only sent for the first message.
-  mark(JVC_HDR_MARK);
-  space(JVC_HDR_SPACE);
+  mark(kJVCHdrMark);
+  space(kJVCHdrSpace);
 
   // We always send the data & footer at least once, hence '<= repeat'.
   for (uint16_t i = 0; i <= repeat; i++) {
     sendGeneric(0, 0,  // No Header
-                JVC_BIT_MARK, JVC_ONE_SPACE,
-                JVC_BIT_MARK, JVC_ZERO_SPACE,
-                JVC_BIT_MARK, JVC_MIN_GAP,
+                kJVCBitMark, kJVCOneSpace,
+                kJVCBitMark, kJVCZeroSpace,
+                kJVCBitMark, kJVCMinGap,
                 data, nbits, 38, true, 0,  // Repeats are handles elsewhere.
                 33);
     // Wait till the end of the repeat time window before we send another code.
     uint32_t elapsed = usecs.elapsed();
     // Avoid potential unsigned integer underflow.
-    // e.g. when elapsed > JVC_RPT_LENGTH.
-    if (elapsed < JVC_RPT_LENGTH)
-      space(JVC_RPT_LENGTH - elapsed);
+    // e.g. when elapsed > kJVCRptLength.
+    if (elapsed < kJVCRptLength)
+      space(kJVCRptLength - elapsed);
     usecs.reset();
   }
 }
@@ -125,36 +125,36 @@ bool IRrecv::decodeJVC(decode_results *results, uint16_t nbits,  bool strict) {
   uint32_t s_tick;
   // Header
   // (Optional as repeat codes don't have the header)
-  if (matchMark(results->rawbuf[offset], JVC_HDR_MARK)) {
+  if (matchMark(results->rawbuf[offset], kJVCHdrMark)) {
     isRepeat = false;
-    m_tick = results->rawbuf[offset++] * kRawTick / JVC_HDR_MARK_TICKS;
+    m_tick = results->rawbuf[offset++] * kRawTick / kJVCHdrMarkTicks;
     if (results->rawlen < 2 * nbits + 4)
       return false;  // Can't possibly be a valid JVC message with a header.
-    if (!matchSpace(results->rawbuf[offset], JVC_HDR_SPACE))
+    if (!matchSpace(results->rawbuf[offset], kJVCHdrSpace))
       return false;
-    s_tick = results->rawbuf[offset++] * kRawTick / JVC_HDR_SPACE_TICKS;
+    s_tick = results->rawbuf[offset++] * kRawTick / kJVCHdrSpaceTicks;
   } else {
     // We can't easily auto-calibrate as there is no header, so assume
     // the default tick time.
-    m_tick = JVC_TICK;
-    s_tick = JVC_TICK;
+    m_tick = kJVCTick;
+    s_tick = kJVCTick;
   }
 
   // Data
   match_result_t data_result = matchData(&(results->rawbuf[offset]), nbits,
-                                         JVC_BIT_MARK_TICKS * m_tick,
-                                         JVC_ONE_SPACE_TICKS * s_tick,
-                                         JVC_BIT_MARK_TICKS * m_tick,
-                                         JVC_ZERO_SPACE_TICKS * s_tick);
+                                         kJVCBitMarkTicks * m_tick,
+                                         kJVCOneSpaceTicks * s_tick,
+                                         kJVCBitMarkTicks * m_tick,
+                                         kJVCZeroSpaceTicks * s_tick);
   if (data_result.success == false) return false;
   data = data_result.data;
   offset += data_result.used;
 
   // Footer
-  if (!matchMark(results->rawbuf[offset++], JVC_BIT_MARK_TICKS * m_tick))
+  if (!matchMark(results->rawbuf[offset++], kJVCBitMarkTicks * m_tick))
     return false;
   if (offset < results->rawlen &&
-      !matchAtLeast(results->rawbuf[offset], JVC_MIN_GAP_TICKS * s_tick))
+      !matchAtLeast(results->rawbuf[offset], kJVCMinGapTicks * s_tick))
     return false;
 
   // Success
