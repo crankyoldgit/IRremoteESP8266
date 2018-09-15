784d93af33bfee42a83060ba6f2f780873fd30df
David Conran <crankyoldgit@users.noreply.github.com>
2018-09-15 01:26:46 -0700

    Experimental support for Lutron protocol. (#516)
    * sendLutron & decodeLutron added.
    * Very basic unit tests for Lutron protocol.
    * IRMQTTServer updated to add Lutron
    * [BUG] Fix HTML menu error for GICABLE in IRMQTTServer
    * Based on information in Issue #515
    
    * Note: This is EXPERIMENTAL/ALPHA code. Based off a single message.

--- src/IRrecv.cpp
+++ src/IRrecv.cpp
@@ -503,6 +503,11 @@ bool IRrecv::decode(decode_results *results, irparams_t *save) {
   if (decodeSamsungAC(results))
     return true;
 #endif
+#if DECODE_LUTRON
+  DPRINTLN("Attempting Lutron decode");
+  if (decodeLutron(results))
+    return true;
+#endif
 #if DECODE_HASH
   // decodeHash returns a hash on any input.
   // Thus, it needs to be last in the list.
