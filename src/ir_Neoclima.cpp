3ee107b0d2c2a5bf9e5f764a254dba4e01e03e75
David <david@xyzzy.com.au>
2019-06-22 01:37:40 +1000

    Experimental basic support for Neoclima protocol. * `sendNeoclima()` & `decodeNeoclima()` added. * Unit tests based on sample data.   - Self decoding works.   - Real data works. * Minor other code cleanup.
    For #764

---
+++ src/ir_Neoclima.cpp
@@ -0,0 +1,98 @@
+// Copyright 2019 David Conran
+
+// Neoclima A/C support
+
+// Supports:
+//   Brand: Neoclima,  Model: NS-09AHTI A/C
+//   Brand: Neoclima,  Model: ZH/TY-01 remote
+
+#include <algorithm>
+#include "IRrecv.h"
+#include "IRsend.h"
+#include "IRutils.h"
+
+// Constants
+
+const uint16_t kNeoclimaHdrMark = 6112;
+const uint16_t kNeoclimaHdrSpace = 7391;
+const uint16_t kNeoclimaBitMark = 537;
+const uint16_t kNeoclimaOneSpace = 1651;
+const uint16_t kNeoclimaZeroSpace = 571;
+const uint32_t kNeoclimaMinGap = kDefaultMessageGap;
+
+#if SEND_NEOCLIMA
+// Send a Neoclima message.
+//
+// Args:
+//   data: message to be sent.
+//   nbytes: Nr. of bytes of the message to be sent.
+//   repeat: Nr. of additional times the message is to be sent.
+//
+// Status: Alpha / Untested.
+//
+// Ref:
+//   https://github.com/markszabo/IRremoteESP8266/issues/764
+void IRsend::sendNeoclima(const unsigned char data[], const uint16_t nbytes,
+                          const uint16_t repeat) {
+  // Set IR carrier frequency
+  enableIROut(38);
+
+  for (uint16_t i = 0; i <= repeat; i++) {
+    sendGeneric(kNeoclimaHdrMark, kNeoclimaHdrSpace,
+                kNeoclimaBitMark, kNeoclimaOneSpace,
+                kNeoclimaBitMark, kNeoclimaZeroSpace,
+                kNeoclimaBitMark, kNeoclimaHdrSpace,
+                data, nbytes, 38, true, 0,  // Repeats are already handled.
+                50);
+     // Extra footer.
+     mark(kNeoclimaBitMark);
+     space(kNeoclimaMinGap);
+  }
+}
+#endif  // SEND_NEOCLIMA
+
+#if DECODE_NEOCLIMA
+// Decode the supplied Neoclima message.
+//
+// Args:
+//   results: Ptr to the data to decode and where to store the decode result.
+//   nbits:   Nr. of data bits to expect. Typically kNeoclimaBits.
+//   strict:  Flag indicating if we should perform strict matching.
+// Returns:
+//   boolean: True if it can decode it, false if it can't.
+//
+// Status: BETA / Probably works
+//
+// Ref:
+//   https://github.com/markszabo/IRremoteESP8266/issues/764
+bool IRrecv::decodeNeoclima(decode_results *results, const uint16_t nbits,
+                            const bool strict) {
+  // Compliance
+  if (strict && nbits != kNeoclimaBits)
+    return false;  // Incorrect nr. of bits per spec.
+
+  uint16_t offset = kStartOffset;
+  // Match Main Header + Data + Footer
+  uint16_t used;
+  used = matchGeneric(results->rawbuf + offset, results->state,
+                      results->rawlen - offset, nbits,
+                      kNeoclimaHdrMark, kNeoclimaHdrSpace,
+                      kNeoclimaBitMark, kNeoclimaOneSpace,
+                      kNeoclimaBitMark, kNeoclimaZeroSpace,
+                      kNeoclimaBitMark, kNeoclimaHdrSpace, false);
+  if (!used) return false;
+  offset += used;
+  // Extra footer.
+  uint64_t unused;
+  if (!matchGeneric(results->rawbuf + offset, &unused,
+                    results->rawlen - offset, 0, 0, 0, 0, 0, 0, 0,
+                    kNeoclimaBitMark, kNeoclimaHdrSpace, true)) return false;
+  // Success
+  results->decode_type = decode_type_t::NEOCLIMA;
+  results->bits = nbits;
+  // No need to record the state as we stored it as we decoded it.
+  // As we use result->state, we don't record value, address, or command as it
+  // is a union data type.
+  return true;
+}
+#endif  // DECODE_NEOCLIMA
---
+++ test/ir_Neoclima_test.cpp
@@ -0,0 +1,97 @@
+// Copyright 2019 David Conran
+
+#include "IRsend.h"
+#include "IRsend_test.h"
+#include "IRrecv.h"
+#include "IRrecv_test.h"
+#include "gtest/gtest.h"
+
+TEST(TestUtils, Housekeeping) {
+  ASSERT_EQ("NEOCLIMA", typeToString(decode_type_t::NEOCLIMA));
+  ASSERT_EQ(decode_type_t::NEOCLIMA, strToDecodeType("NEOCLIMA"));
+  ASSERT_TRUE(hasACState(decode_type_t::NEOCLIMA));
+}
+
+// Test sending typical data only.
+TEST(TestSendNeoclima, SendDataOnly) {
+  IRsendTest irsend(0);
+  irsend.begin();
+
+  uint8_t state[kNeoclimaStateLength] = {
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x00, 0x56, 0x00, 0x54, 0xA5, 0x9C};
+  irsend.reset();
+  irsend.sendNeoclima(state);
+  EXPECT_EQ(
+      "f38000d50"
+      "m6112s7391"
+      "m537s571m537s571m537s571m537s571m537s571m537s571m537s571m537s571"
+      "m537s571m537s571m537s571m537s571m537s571m537s571m537s571m537s571"
+      "m537s571m537s571m537s571m537s571m537s571m537s571m537s571m537s571"
+      "m537s571m537s571m537s571m537s571m537s571m537s571m537s571m537s571"
+      "m537s571m537s571m537s571m537s571m537s571m537s571m537s571m537s571"
+      "m537s571m537s571m537s571m537s571m537s571m537s571m537s571m537s571"
+      "m537s571m537s571m537s571m537s571m537s571m537s571m537s571m537s571"
+      "m537s571m537s1651m537s571m537s1651m537s571m537s1651m537s1651m537s571"
+      "m537s571m537s571m537s571m537s571m537s571m537s571m537s571m537s571"
+      "m537s571m537s1651m537s571m537s1651m537s571m537s1651m537s571m537s571"
+      "m537s1651m537s571m537s1651m537s571m537s571m537s1651m537s571m537s1651"
+      "m537s1651m537s571m537s571m537s1651m537s1651m537s1651m537s571m537s571"
+      "m537s7391"
+      "m537s100000",
+      irsend.outputStr());
+}
+
+// https://github.com/markszabo/IRremoteESP8266/issues/764#issuecomment-503755096
+TEST(TestDecodeNeoclima, RealExample) {
+  IRsendTest irsend(0);
+  IRrecv irrecv(0);
+  uint16_t rawData[197] = {
+      6112, 7392, 540, 602, 516, 578, 522, 604, 540, 554, 540, 554, 540, 576,
+      518, 576, 516, 554, 540, 608, 542, 554, 540, 554, 540, 576, 518, 604, 516,
+      556, 540, 576, 546, 580, 542, 578, 542, 602, 518, 554, 542, 554, 568, 582,
+      540, 554, 540, 582, 540, 578, 518, 582, 542, 576, 544, 530, 566, 534, 562,
+      534, 562, 552, 542, 582, 540, 604, 518, 608, 542, 554, 540, 582, 540, 604,
+      518, 580, 540, 606, 544, 554, 542, 554, 542, 580, 542, 576, 520, 554, 540,
+      578, 518, 578, 518, 582, 544, 552, 570, 580, 544, 580, 542, 554, 542, 604,
+      520, 576, 520, 580, 540, 556, 540, 556, 542, 584, 566, 580, 542, 1622,
+      542, 554, 542, 1620, 544, 604, 520, 1642, 518, 1674, 548, 560, 564, 580,
+      544, 554, 544, 552, 544, 554, 542, 556, 542, 576, 522, 554, 542, 556, 542,
+      580, 542, 1670, 520, 578, 520, 1622, 542, 580, 518, 1646, 520, 558, 568,
+      552, 546, 1628, 566, 580, 544, 1668, 522, 576, 520, 578, 520, 1670, 522,
+      576, 522, 1670, 496, 1676, 570, 560, 566, 532, 564, 1648, 544, 1670, 522,
+      1650, 544, 552, 544, 576, 520, 7390, 544};  // UNKNOWN EE182D95
+
+
+  uint8_t expectedState[kNeoclimaStateLength] = {
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x00, 0x56, 0x00, 0x54, 0xA5, 0x9C};
+
+  irsend.begin();
+  irsend.reset();
+  irsend.sendRaw(rawData, 197, 38000);
+  irsend.makeDecodeResult();
+  ASSERT_TRUE(irrecv.decode(&irsend.capture));
+  ASSERT_EQ(decode_type_t::NEOCLIMA, irsend.capture.decode_type);
+  ASSERT_EQ(kNeoclimaBits, irsend.capture.bits);
+  EXPECT_STATE_EQ(expectedState, irsend.capture.state, irsend.capture.bits);
+}
+
+// Self decode.
+TEST(TestDecodeNeoclima, SyntheticExample) {
+  IRsendTest irsend(0);
+  IRrecv irrecv(0);
+
+  uint8_t expectedState[kNeoclimaStateLength] = {
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x00, 0x56, 0x00, 0x54, 0xA5, 0x9C};
+
+  irsend.begin();
+  irsend.reset();
+  irsend.sendNeoclima(expectedState);
+  irsend.makeDecodeResult();
+  ASSERT_TRUE(irrecv.decode(&irsend.capture));
+  ASSERT_EQ(decode_type_t::NEOCLIMA, irsend.capture.decode_type);
+  ASSERT_EQ(kNeoclimaBits, irsend.capture.bits);
+  EXPECT_STATE_EQ(expectedState, irsend.capture.state, irsend.capture.bits);
+}
