14138fd4545632e7511c47d5ea8bc32794828fcc
David <david@xyzzy.com.au>
2019-09-28 22:48:39 +1000

    Improvements to setBits() and GETBIT*() handling.

--- src/IRutils.cpp
+++ src/IRutils.cpp
@@ -1131,15 +1131,34 @@ namespace irutils {
   //   dst: Ptr to the uint8_t to be changed.
   //   offset: Nr. of bits from the Least Significant Bit to be ignored.
   //   nbits: Nr of bits of `data` to be placed into the destination uint8_t.
-  //   data: Value to be placed into the byte.
+  //   data: Value to be placed into dst.
   void setBits(uint8_t * const dst, const uint8_t offset, const uint8_t nbits,
                const uint8_t data) {
-    if (offset >= 8 || !nbits) return;  // Short circuit as nothing will change.
+    if (offset >= 8 || !nbits) return;  // Short circuit as it won't change.
     // Calculate the mask for the supplied value.
     uint8_t mask = UINT8_MAX >> (8 - ((nbits > 8) ? 8 : nbits));
     // Calculate the mask & clear the space for the data.
     // Clear the destination bits.
     *dst &= ~(uint8_t)(mask << offset);
+    // Merge in the data.
+    *dst |= ((data & mask) << offset);
+  }
+
+  // Change the uint32_t pointed to by `dst` starting at the `offset`th bit
+  //   and for `nbits` bits, with the contents of `data`.
+  // Args:
+  //   dst: Ptr to the uint32_t to be changed.
+  //   offset: Nr. of bits from the Least Significant Bit to be ignored.
+  //   nbits: Nr of bits of `data` to be placed into the destination uint32_t.
+  //   data: Value to be placed into dst.
+  void setBits(uint32_t * const dst, const uint8_t offset, const uint8_t nbits,
+               const uint32_t data) {
+    if (offset >= 32 || !nbits) return;  // Short circuit as it won't change.
+    // Calculate the mask for the supplied value.
+    uint32_t mask = UINT32_MAX >> (32 - ((nbits > 32) ? 32 : nbits));
+    // Calculate the mask & clear the space for the data.
+    // Clear the destination bits.
+    *dst &= ~(mask << offset);
     // Merge in the data.
     *dst |= ((data & mask) << offset);
   }
@@ -1150,10 +1169,10 @@ namespace irutils {
   //   dst: Ptr to the uint64_t to be changed.
   //   offset: Nr. of bits from the Least Significant Bit to be ignored.
   //   nbits: Nr of bits of `data` to be placed into the destination uint64_t.
-  //   data: Value to be placed into the byte.
+  //   data: Value to be placed into dst.
   void setBits(uint64_t * const dst, const uint8_t offset, const uint8_t nbits,
                const uint64_t data) {
-    if (offset >= 64 || !nbits) return;  // Short circuit as nothing will change.
+    if (offset >= 64 || !nbits) return;  // Short circuit as it won't change.
     // Calculate the mask for the supplied value.
     uint64_t mask = UINT64_MAX >> (64 - ((nbits > 64) ? 64 : nbits));
     // Calculate the mask & clear the space for the data.
--- src/IRutils.h
+++ src/IRutils.h
@@ -14,6 +14,10 @@
 #include "IRremoteESP8266.h"
 #include "IRrecv.h"
 
+const uint8_t kNibbleSize = 4;
+const uint8_t kLowNibble = 0;
+const uint8_t kHighNibble = 4;
+const uint8_t kModeBitsSize = 3;
 uint64_t reverseBits(uint64_t input, uint16_t nbits);
 String uint64ToString(uint64_t input, uint8_t base = 10);
 String typeToString(const decode_type_t protocol,
@@ -72,6 +76,17 @@ namespace irutils {
 #define GETBIT16(a, b) (a & ((uint16_t)1 << b))
 #define GETBIT32(a, b) (a & ((uint32_t)1 << b))
 #define GETBIT64(a, b) (a & ((uint64_t)1 << b))
+#define GETBITS8(data, offset, size) \
+    (((data) & (((uint8_t)UINT8_MAX >> (8 - (size))) << (offset))) >> (offset))
+#define GETBITS16(data, offset, size) \
+    (((data) & (((uint16_t)UINT16_MAX >> (16 - (size))) << (offset))) >> \
+     (offset))
+#define GETBITS32(data, offset, size) \
+    (((data) & (((uint32_t)UINT32_MAX >> (32 - (size))) << (offset))) >> \
+     (offset))
+#define GETBITS64(data, offset, size) \
+    (((data) & (((uint64_t)UINT64_MAX >> (64 - (size))) << (offset))) >> \
+     (offset))
   uint64_t setBit(const uint64_t data, const uint8_t position,
                   const bool on = true, const uint8_t size = 64);
   uint8_t setBit(const uint8_t data, const uint8_t position,
@@ -84,7 +99,9 @@ namespace irutils {
               const bool on = true);
   void setBits(uint8_t * const dst, const uint8_t offset, const uint8_t nbits,
                const uint8_t data);
+  void setBits(uint32_t * const dst, const uint8_t offset, const uint8_t nbits,
+               const uint32_t data);
   void setBits(uint64_t * const dst, const uint8_t offset, const uint8_t nbits,
-              const uint64_t data);
+               const uint64_t data);
 }  // namespace irutils
 #endif  // IRUTILS_H_
