a6904cad9dadcf6050efc7b0e81df664d45b6857
David <david@xyzzy.com.au>
2019-10-07 09:10:44 +1000

    Remove some unchanged entries and fix lint issues.

--- src/locale/de_DE.h
+++ src/locale/de_DE.h
@@ -1,3 +1,4 @@
+// Copyright 2019 - Martin (@finfinack)
 // Locale/language file for German / Germany.
 // This file will override the default values located in `defaults.h`.
 #ifndef LOCALE_DE_DE_H_
@@ -5,21 +6,15 @@
 
 #define D_STR_UNKNOWN "UNBEKANNT"
 #define D_STR_PROTOCOL "Protokoll"
-#define D_STR_POWER "Power"
 #define D_STR_ON "Ein"
 #define D_STR_OFF "Aus"
 #define D_STR_MODE "Modus"
 #define D_STR_TOGGLE "Umschalten"
-#define D_STR_TURBO "Turbo"
-#define D_STR_SUPER "Super"
 #define D_STR_SLEEP "Schlafen"
 #define D_STR_LIGHT "Licht"
 #define D_STR_POWERFUL "Stark"
 #define D_STR_QUIET "Ruhig"
 #define D_STR_ECONO "Eco"
-#define D_STR_SWING "Swing"
-#define D_STR_SWINGH D_STR_SWING"(H)"
-#define D_STR_SWINGV D_STR_SWING"(V)"
 #define D_STR_BEEP "Piep"
 #define D_STR_MOLD "Schimmel"
 #define D_STR_CLEAN "Reinigen"
@@ -29,41 +24,28 @@
 #define D_STR_OFFTIMER D_STR_OFF " " D_STR_TIMER
 #define D_STR_CLOCK "Uhr"
 #define D_STR_COMMAND "Befehl"
-//#define D_STR_XFAN "XFan"
 #define D_STR_HEALTH "Gesundheit"
-#define D_STR_MODEL "Model"
 #define D_STR_TEMP "Temp"
-//#define D_STR_IFEEL "IFeel"
 #define D_STR_HUMID "Feucht"
 #define D_STR_SAVE "Speichern"
 #define D_STR_EYE "Auge"
 #define D_STR_FOLLOW "Folgen"
-#define D_STR_ION "Ion"
 #define D_STR_FRESH "Frisch"
 #define D_STR_HOLD "Halten"
-#define D_STR_8C_HEAT "8C " D_STR_HEAT
 #define D_STR_BUTTON "Knopf"
 #define D_STR_NIGHT "Nacht"
 #define D_STR_SILENT "Ruhig"
-#define D_STR_FILTER "Filter"
-#define D_STR_3D "3D"
-#define D_STR_CELSIUS "Celsius"
 #define D_STR_UP "Hinauf"
 #define D_STR_TEMPUP D_STR_TEMP " " D_STR_UP
 #define D_STR_DOWN "Hinunter"
 #define D_STR_TEMPDOWN D_STR_TEMP " " D_STR_DOWN
 #define D_STR_CHANGE "Wechseln"
-#define D_STR_START "Start"
-#define D_STR_STOP "Stop"
 #define D_STR_MOVE "Verschieben"
 #define D_STR_SET "Setzen"
 #define D_STR_CANCEL "Abbrechen"
 #define D_STR_COMFORT "Komfort"
-#define D_STR_SENSOR "Sensor"
 #define D_STR_WEEKLY "Wöchentlich"
 #define D_STR_WEEKLYTIMER D_STR_WEEKLY " " D_STR_TIMER
-#define D_STR_WIFI "WiFi"
-#define D_STR_LAST "Last"
 #define D_STR_FAST "Schnell"
 #define D_STR_SLOW "Langsam"
 #define D_STR_AIRFLOW "Luftzug"
@@ -79,7 +61,6 @@
 #define D_STR_WALL "Wand"
 #define D_STR_ROOM "Raum"
 #define D_STR_6THSENSE "6ter Sinn"
-//#define D_STR_ZONEFOLLOW "Zone Follow"
 #define D_STR_FIXED "Fixiert"
 
-#endif  // LOCALE_DE_DE_H__
+#endif  // LOCALE_DE_DE_H_
