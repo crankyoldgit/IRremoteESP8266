c23e460553f6b16c1f8bd1af42cdc170a7f5d900
charlieyv <carloshernandezh@gmail.com>
2019-10-07 13:15:03 +0200

    Create es_ES.h
    Adding spanish version of the locale file.

---
+++ src/locale/es_ES.h
@@ -0,0 +1,429 @@
+// Copyright 2019 - Carlos (@finfinack)
+// Locale/language file for Spanish / Spain.
+// This file will override the default values located in `defaults.h`.
+#ifndef LOCALE_DEFAULTS_H_
+#define LOCALE_DEFAULTS_H_
+
+#ifndef D_STR_UNKNOWN
+#define D_STR_UNKNOWN "DESCONOCIDO"
+#endif  // D_STR_UNKNOWN
+#ifndef D_STR_PROTOCOL
+#define D_STR_PROTOCOL "Protocolo"
+#endif  // D_STR_PROTOCOL
+#ifndef D_STR_POWER
+#define D_STR_POWER "Poder"
+#endif  // D_STR_POWER
+#ifndef D_STR_ON
+#define D_STR_ON "Encendido"
+#endif  // D_STR_ON
+#ifndef D_STR_OFF
+#define D_STR_OFF "Apagado"
+#endif  // D_STR_OFF
+#ifndef D_STR_MODE
+#define D_STR_MODE "Modo"
+#endif  // D_STR_MODE
+#ifndef D_STR_TOGGLE
+#define D_STR_TOGGLE "Palanca"
+#endif  // D_STR_TOGGLE
+#ifndef D_STR_TURBO
+#define D_STR_TURBO "Turbo"
+#endif  // D_STR_TURBO
+#ifndef D_STR_SUPER
+#define D_STR_SUPER "Super"
+#endif  // D_STR_SUPER
+#ifndef D_STR_SLEEP
+#define D_STR_SLEEP "Dormir"
+#endif  // D_STR_SLEEP
+#ifndef D_STR_LIGHT
+#define D_STR_LIGHT "Luz"
+#endif  // D_STR_LIGHT
+#ifndef D_STR_POWERFUL
+#define D_STR_POWERFUL "Poderoso"
+#endif  // D_STR_POWERFUL
+#ifndef D_STR_QUIET
+#define D_STR_QUIET "Silencio"
+#endif  // D_STR_QUIET
+#ifndef D_STR_ECONO
+#define D_STR_ECONO "Econo"
+#endif  // D_STR_ECONO
+#ifndef D_STR_SWING
+#define D_STR_SWING "Oscilar"
+#endif  // D_STR_SWING
+#ifndef D_STR_SWINGH
+#define D_STR_SWINGH D_STR_SWING"(H)"
+#endif  // D_STR_SWINGH
+#ifndef D_STR_SWINGV
+#define D_STR_SWINGV D_STR_SWING"(V)"
+#endif  // D_STR_SWINGV
+#ifndef D_STR_BEEP
+#define D_STR_BEEP "Bip"
+#endif  // D_STR_BEEP
+#ifndef D_STR_MOLD
+#define D_STR_MOLD "Molde"
+#endif  // D_STR_MOLD
+#ifndef D_STR_CLEAN
+#define D_STR_CLEAN "Limpiar"
+#endif  // D_STR_CLEAN
+#ifndef D_STR_PURIFY
+#define D_STR_PURIFY "Purificar"
+#endif  // D_STR_PURIFY
+#ifndef D_STR_TIMER
+#define D_STR_TIMER "Temporizador"
+#endif  // D_STR_TIMER
+#ifndef D_STR_ONTIMER
+#define D_STR_ONTIMER D_STR_ON " " D_STR_TIMER
+#endif  // D_STR_ONTIMER
+#ifndef D_STR_OFFTIMER
+#define D_STR_OFFTIMER D_STR_OFF " " D_STR_TIMER
+#endif  // D_STR_OFFTIMER
+#ifndef D_STR_CLOCK
+#define D_STR_CLOCK "Reloj"
+#endif  // D_STR_CLOCK
+#ifndef D_STR_COMMAND
+#define D_STR_COMMAND "Comando"
+#endif  // D_STR_COMMAND
+#ifndef D_STR_XFAN
+#define D_STR_XFAN "XFan"
+#endif  // D_STR_XFAN
+#ifndef D_STR_HEALTH
+#define D_STR_HEALTH "Salud"
+#endif  // D_STR_HEALTH
+#ifndef D_STR_MODEL
+#define D_STR_MODEL "Modelo"
+#endif  // D_STR_MODEL
+#ifndef D_STR_TEMP
+#define D_STR_TEMP "Temperatura"
+#endif  // D_STR_TEMP
+#ifndef D_STR_IFEEL
+#define D_STR_IFEEL "IFeel"
+#endif  // D_STR_IFEEL
+#ifndef D_STR_HUMID
+#define D_STR_HUMID "Humedo"
+#endif  // D_STR_HUMID
+#ifndef D_STR_SAVE
+#define D_STR_SAVE "Guardar"
+#endif  // D_STR_SAVE
+#ifndef D_STR_EYE
+#define D_STR_EYE "Ojo"
+#endif  // D_STR_EYE
+#ifndef D_STR_FOLLOW
+#define D_STR_FOLLOW "Seguir"
+#endif  // D_STR_FOLLOW
+#ifndef D_STR_ION
+#define D_STR_ION "Ion"
+#endif  // D_STR_ION
+#ifndef D_STR_FRESH
+#define D_STR_FRESH "Fresco"
+#endif  // D_STR_FRESH
+#ifndef D_STR_HOLD
+#define D_STR_HOLD "Mantener"
+#endif  // D_STR_HOLD
+#ifndef D_STR_8C_HEAT
+#define D_STR_8C_HEAT "8C " D_STR_HEAT
+#endif  // D_STR_8C_HEAT
+#ifndef D_STR_BUTTON
+#define D_STR_BUTTON "Boton"
+#endif  // D_STR_BUTTON
+#ifndef D_STR_NIGHT
+#define D_STR_NIGHT "Noche"
+#endif  // D_STR_NIGHT
+#ifndef D_STR_SILENT
+#define D_STR_SILENT "Silencio"
+#endif  // D_STR_SILENT
+#ifndef D_STR_FILTER
+#define D_STR_FILTER "Filtro"
+#endif  // D_STR_FILTER
+#ifndef D_STR_3D
+#define D_STR_3D "3D"
+#endif  // D_STR_3D
+#ifndef D_STR_CELSIUS
+#define D_STR_CELSIUS "Celsius"
+#endif  // D_STR_CELSIUS
+#ifndef D_STR_UP
+#define D_STR_UP "Arriba"
+#endif  // D_STR_UP
+#ifndef D_STR_TEMPUP
+#define D_STR_TEMPUP D_STR_TEMP " " D_STR_UP
+#endif  // D_STR_TEMPUP
+#ifndef D_STR_DOWN
+#define D_STR_DOWN "Abajo"
+#endif  // D_STR_DOWN
+#ifndef D_STR_TEMPDOWN
+#define D_STR_TEMPDOWN D_STR_TEMP " " D_STR_DOWN
+#endif  // D_STR_TEMPDOWN
+#ifndef D_STR_CHANGE
+#define D_STR_CHANGE "Cambiar"
+#endif  // D_STR_CHANGE
+#ifndef D_STR_START
+#define D_STR_START "Comenzar"
+#endif  // D_STR_START
+#ifndef D_STR_STOP
+#define D_STR_STOP "Parar"
+#endif  // D_STR_STOP
+#ifndef D_STR_MOVE
+#define D_STR_MOVE "Mover"
+#endif  // D_STR_MOVE
+#ifndef D_STR_SET
+#define D_STR_SET "Fijar"
+#endif  // D_STR_SET
+#ifndef D_STR_CANCEL
+#define D_STR_CANCEL "Cancelar"
+#endif  // D_STR_CANCEL
+#ifndef D_STR_COMFORT
+#define D_STR_COMFORT "Comodo"
+#endif  // D_STR_COMFORT
+#ifndef D_STR_SENSOR
+#define D_STR_SENSOR "Sensor"
+#endif  // D_STR_SENSOR
+#ifndef D_STR_WEEKLY
+#define D_STR_WEEKLY "Semanal"
+#endif  // D_STR_WEEKLY
+#ifndef D_STR_WEEKLYTIMER
+#define D_STR_WEEKLYTIMER D_STR_WEEKLY " " D_STR_TIMER
+#endif  // D_STR_WEEKLYTIMER
+#ifndef D_STR_WIFI
+#define D_STR_WIFI "WiFi"
+#endif  // D_STR_WIFI
+#ifndef D_STR_LAST
+#define D_STR_LAST "Ultimo"
+#endif  // D_STR_LAST
+#ifndef D_STR_FAST
+#define D_STR_FAST "Rapido"
+#endif  // D_STR_FAST
+#ifndef D_STR_SLOW
+#define D_STR_SLOW "Lento"
+#endif  // D_STR_SLOW
+#ifndef D_STR_AIRFLOW
+#define D_STR_AIRFLOW "Flujo de Aire"
+#endif  // D_STR_AIRFLOW
+#ifndef D_STR_STEP
+#define D_STR_STEP "Paso"
+#endif  // D_STR_STEP
+#ifndef D_STR_NA
+#define D_STR_NA "N/A"
+#endif  // D_STR_NA
+#ifndef D_STR_OUTSIDE
+#define D_STR_OUTSIDE "Afuera"
+#endif  // D_STR_OUTSIDE
+#ifndef D_STR_LOUD
+#define D_STR_LOUD "Ruidoso"
+#endif  // D_STR_LOUD
+#ifndef D_STR_UPPER
+#define D_STR_UPPER "Superior"
+#endif  // D_STR_UPPER
+#ifndef D_STR_LOWER
+#define D_STR_LOWER "Inferior"
+#endif  // D_STR_LOWER
+#ifndef D_STR_BREEZE
+#define D_STR_BREEZE "Brisa"
+#endif  // D_STR_BREEZE
+#ifndef D_STR_CIRCULATE
+#define D_STR_CIRCULATE "Circular"
+#endif  // D_STR_CIRCULATE
+#ifndef D_STR_CEILING
+#define D_STR_CEILING "Techo"
+#endif  // D_STR_CEILING
+#ifndef D_STR_WALL
+#define D_STR_WALL "Pared"
+#endif  // D_STR_WALL
+#ifndef D_STR_ROOM
+#define D_STR_ROOM "Cuarto"
+#endif  // D_STR_ROOM
+#ifndef D_STR_6THSENSE
+#define D_STR_6THSENSE "6to. Sentido"
+#endif  // D_STR_6THSENSE
+#ifndef D_STR_ZONEFOLLOW
+#define D_STR_ZONEFOLLOW "Zona Seguir"
+#endif  // D_STR_ZONEFOLLOW
+#ifndef D_STR_FIXED
+#define D_STR_FIXED "Fijo"
+#endif  // D_STR_FIXED
+
+#ifndef D_STR_AUTO
+#define D_STR_AUTO "Auto"
+#endif  // D_STR_AUTO
+#ifndef D_STR_AUTOMATIC
+#define D_STR_AUTOMATIC "Automatico"
+#endif  // D_STR_AUTOMATIC
+#ifndef D_STR_MANUAL
+#define D_STR_MANUAL "Manual"
+#endif  // D_STR_MANUAL
+#ifndef D_STR_COOL
+#define D_STR_COOL "Frio"
+#endif  // D_STR_COOL
+#ifndef D_STR_HEAT
+#define D_STR_HEAT "Calor"
+#endif  // D_STR_HEAT
+#ifndef D_STR_FAN
+#define D_STR_FAN "Ventilador"
+#endif  // D_STR_FAN
+#ifndef D_STR_FANONLY
+#define D_STR_FANONLY "ventilador_solamente"
+#endif  // D_STR_FANONLY
+#ifndef D_STR_DRY
+#define D_STR_DRY "Seco"
+#endif  // D_STR_DRY
+
+#ifndef D_STR_MAX
+#define D_STR_MAX "Max"
+#endif  // D_STR_MAX
+#ifndef D_STR_MAXIMUM
+#define D_STR_MAXIMUM "Maximo"
+#endif  // D_STR_MAXIMUM
+#ifndef D_STR_MIN
+#define D_STR_MIN "Min"
+#endif  // D_STR_MIN
+#ifndef D_STR_MINIMUM
+#define D_STR_MINIMUM "Minimo"
+#endif  // D_STR_MINIMUM
+#ifndef D_STR_MED
+#define D_STR_MED "Med"
+#endif  // D_STR_MED
+#ifndef D_STR_MEDIUM
+#define D_STR_MEDIUM "Medio"
+#endif  // D_STR_MEDIUM
+
+#ifndef D_STR_HIGHEST
+#define D_STR_HIGHEST "Mas Alto"
+#endif  // D_STR_HIGHEST
+#ifndef D_STR_HIGH
+#define D_STR_HIGH "Alto"
+#endif  // D_STR_HIGH
+#ifndef D_STR_HI
+#define D_STR_HI "Alto"
+#endif  // D_STR_HI
+#ifndef D_STR_MID
+#define D_STR_MID "Medio"
+#endif  // D_STR_MID
+#ifndef D_STR_MIDDLE
+#define D_STR_MIDDLE "Medio"
+#endif  // D_STR_MIDDLE
+#ifndef D_STR_LOW
+#define D_STR_LOW "Bajo"
+#endif  // D_STR_LOW
+#ifndef D_STR_LO
+#define D_STR_LO "Bajo"
+#endif  // D_STR_LO
+#ifndef D_STR_LOWEST
+#define D_STR_LOWEST "Mas Bajo"
+#endif  // D_STR_LOWEST
+#ifndef D_STR_RIGHT
+#define D_STR_RIGHT "Derecha"
+#endif  // D_STR_RIGHT
+#ifndef D_STR_MAXRIGHT
+#define D_STR_MAXRIGHT D_STR_MAX " " D_STR_RIGHT
+#endif  // D_STR_MAXRIGHT
+#ifndef D_STR_RIGHTMAX_NOSPACE
+#define D_STR_RIGHTMAX_NOSPACE D_STR_RIGHT D_STR_MAX
+#endif  // D_STR_RIGHTMAX_NOSPACE
+#ifndef D_STR_LEFT
+#define D_STR_LEFT "Izquierda"
+#endif  // D_STR_LEFT
+#ifndef D_STR_MAXLEFT
+#define D_STR_MAXLEFT D_STR_MAX " " D_STR_LEFT
+#endif  // D_STR_MAXLEFT
+#ifndef D_STR_LEFTMAX_NOSPACE
+#define D_STR_LEFTMAX_NOSPACE D_STR_LEFT D_STR_MAX
+#endif  // D_STR_LEFTMAX_NOSPACE
+#ifndef D_STR_WIDE
+#define D_STR_WIDE "Ancho"
+#endif  // D_STR_WIDE
+#ifndef D_STR_CENTRE
+#define D_STR_CENTRE "Centro"
+#endif  // D_STR_CENTRE
+#ifndef D_STR_TOP
+#define D_STR_TOP "Tope"
+#endif  // D_STR_TOP
+#ifndef D_STR_BOTTOM
+#define D_STR_BOTTOM "Fondo"
+#endif  // D_STR_BOTTOM
+
+#ifndef D_STR_SPACELBRACE
+#define D_STR_SPACELBRACE " ("
+#endif  // D_STR_SPACELBRACE
+#ifndef D_STR_COMMASPACE
+#define D_STR_COMMASPACE ", "
+#endif  // D_STR_COMMASPACE
+#ifndef D_STR_COLONSPACE
+#define D_STR_COLONSPACE ": "
+#endif  // D_STR_COLONSPACE
+
+#ifndef D_CHR_TIME_SEP
+#define D_CHR_TIME_SEP ':'
+#endif  // D_CHR_TIME_SEP
+#ifndef D_STR_DAY
+#define D_STR_DAY "Dia"
+#endif  // D_STR_DAY
+#ifndef D_STR_DAYS
+#define D_STR_DAYS D_STR_DAY "s"
+#endif  // D_STR_DAYS
+#ifndef D_STR_HOUR
+#define D_STR_HOUR "Hora"
+#endif  // D_STR_HOUR
+#ifndef D_STR_HOURS
+#define D_STR_HOURS D_STR_HOUR "s"
+#endif  // D_STR_HOURS
+#ifndef D_STR_MINUTE
+#define D_STR_MINUTE "Minuto"
+#endif  // D_STR_MINUTE
+#ifndef D_STR_MINUTES
+#define D_STR_MINUTES D_STR_MINUTE "s"
+#endif  // D_STR_MINUTES
+#ifndef D_STR_SECOND
+#define D_STR_SECOND "Segundo"
+#endif  // D_STR_SECOND
+#ifndef D_STR_SECONDS
+#define D_STR_SECONDS D_STR_SECOND "s"
+#endif  // D_STR_SECONDS
+#ifndef D_STR_NOW
+#define D_STR_NOW "Ahora"
+#endif  // D_STR_NOW
+#ifndef D_STR_THREELETTERDAYS
+#define D_STR_THREELETTERDAYS "DomLunMarMieJueVieSab"
+#endif  // D_STR_THREELETTERDAYS
+
+#ifndef D_STR_YES
+#define D_STR_YES "Si"
+#endif  // D_STR_YES
+#ifndef D_STR_NO
+#define D_STR_NO "No"
+#endif  // D_STR_NO
+#ifndef D_STR_TRUE
+#define D_STR_TRUE "Cierto"
+#endif  // D_STR_TRUE
+#ifndef D_STR_FALSE
+#define D_STR_FALSE "Falso"
+#endif  // D_STR_FALSE
+
+#ifndef D_STR_REPEAT
+#define D_STR_REPEAT "Repetir"
+#endif  // D_STR_REPEAT
+#ifndef D_STR_CODE
+#define D_STR_CODE "Codigo"
+#endif  // D_STR_CODE
+#ifndef D_STR_BITS
+#define D_STR_BITS "Bits"
+#endif  // D_STR_BITS
+
+// IRrecvDumpV2
+#ifndef D_STR_TIMESTAMP
+#define D_STR_TIMESTAMP "marca de tiempo"
+#endif  // D_STR_TIMESTAMP
+#ifndef D_STR_LIBRARY
+#define D_STR_LIBRARY "Libreria"
+#endif  // D_STR_LIBRARY
+#ifndef D_STR_MESGDESC
+#define D_STR_MESGDESC "Mesg Desc."
+#endif  // D_STR_MESGDESC
+#ifndef D_STR_IRRECVDUMP_STARTUP
+#define D_STR_IRRECVDUMP_STARTUP \
+    "IRrecvDumpV2 esta ahora corriendo y esperando por comando IR en Pin %d"
+#endif  // D_STR_IRRECVDUMP_STARTUP
+#ifndef D_WARN_BUFFERFULL
+#define D_WARN_BUFFERFULL \
+    "WARNING: Codigo IR es muy grande para el buffer (>= %d). "\
+    "Este resultando no debe ser reconocido hasta que esto sea resuelto." \
+    "Edite & incremente `kCaptureBufferSize`."
+#endif  // D_WARN_BUFFERFULL
+
+#endif  // LOCALE_DEFAULTS_H_
