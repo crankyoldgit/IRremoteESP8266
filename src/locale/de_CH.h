f5b0829d1d6eb6e264a4f926f901ed10302eb773
Martin <finfinack@users.noreply.github.com>
2019-10-07 09:21:44 +0200

    Create de_CH locale
    This creates a default de_CH locale based on the de_DE one. For now, no additional overwrites.

---
+++ src/locale/de_CH.h
@@ -0,0 +1,10 @@
+// Copyright 2019 - Martin (@finfinack)
+// Locale/language file for German / Switzerland.
+// This file will override the default values located in `defaults.h`.
+#ifndef LOCALE_DE_CH_H_
+#define LOCALE_DE_CH_H_
+
+// Import German / Germany as default overwrites.
+#include "locale/de_DE.h"
+
+#endif  // LOCALE_DE_CH_H_
