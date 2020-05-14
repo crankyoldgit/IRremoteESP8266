930b289df725bcbf8a94e2f6082057811e85c44c
David <david@xyzzy.com.au>
2020-05-15 09:35:27 +1000

    Improve raw_to_pronto_code.py * Use lower-case hex code output for pronto codes. * Add option to send the raw code via the repeat section of the Pronto code, rather than the inital section. * Add synonym of `-g` for the gap arg.
    For #1103

--- tools/raw_to_pronto_code.py
+++ tools/raw_to_pronto_code.py
@@ -7,7 +7,9 @@ import sys
 from auto_analyse_raw_data import convert_rawdata, add_rawdata_args, get_rawdata
 
 
-def parse_and_report(rawdata_str, hertz, end_usecs, verbose, output=sys.stdout):
+# pylint: disable=too-many-arguments
+def parse_and_report(rawdata_str, hertz, end_usecs, use_repeat, verbose,
+                     output=sys.stdout):
   """Analyse the rawdata c++ definition of a IR message."""
 
   # Parse the input.
@@ -29,15 +31,20 @@ def parse_and_report(rawdata_str, hertz, end_usecs, verbose, output=sys.stdout):
   if verbose:
     output.write("Pronto period is %f uSecs.\n" % period)
   # Add the lengths to the code.
-  result.append("%04X" % int(len(rawdata) / 2))  # Initial burst
-  result.append("%04X" % 0)  # Repeat (not used by this program)
+  if use_repeat:
+    result.append("%04x" % 0)  # No Initial burst
+    result.append("%04x" % int(len(rawdata) / 2))  # Repeat code length
+  else:
+    result.append("%04x" % int(len(rawdata) / 2))  # Initial burst code length
+    result.append("%04x" % 0)  # No Repeat
 
   # Add the data.
   if verbose:
     output.write("Raw data: %s " % rawdata)
   for i in rawdata:
-    result.append("%04X" % int(i / period))
+    result.append("%04x" % int(i / period))
   output.write("Pronto code = '%s'\n" % " ".join(result))
+# pylint: enable=too-many-arguments
 
 
 def main():
@@ -54,6 +61,7 @@ def main():
       dest="hertz",
       required=True)
   arg_parser.add_argument(
+      "-g",
       "--gap",
       "--endgap",
       type=int,
@@ -67,10 +75,17 @@ def main():
       action="store_true",
       dest="verbose",
       default=False)
+  arg_parser.add_argument(
+      "-r",
+      "--repeat_code",
+      action='store_true',
+      help="Send using the 'repeat' section of the pronto code.",
+      dest="use_repeat")
   add_rawdata_args(arg_parser)
   arg_options = arg_parser.parse_args()
   parse_and_report(get_rawdata(arg_options), arg_options.hertz,
-                   arg_options.usecs, arg_options.verbose)
+                   arg_options.usecs, arg_options.use_repeat,
+                   arg_options.verbose)
 
 
 if __name__ == '__main__':
