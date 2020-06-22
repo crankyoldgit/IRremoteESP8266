da2c82798a8b5dd147f1fd37c1f9e3b51af685e0
David <david@xyzzy.com.au>
2020-06-22 19:49:07 +1000

    Regenerate Doxygen documentation

--- docs/doxygen/html/search/variables_7.js
+++ docs/doxygen/html/search/variables_7.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['heat_5fmode_4267',['heat_mode',['../classIRArgoAC.html#a255762f71502b9ffeb0686759991ec53',1,'IRArgoAC']]]
+  ['heat_5fmode_4395',['heat_mode',['../classIRArgoAC.html#a255762f71502b9ffeb0686759991ec53',1,'IRArgoAC']]]
 ];
