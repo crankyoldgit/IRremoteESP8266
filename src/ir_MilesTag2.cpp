22fc1775592cf3b8636d86ef632dfa8f29e0deaa
vitos1k <vitos1g@gmail.com>
2021-01-14 16:01:02 +0300

    As suggested, added MilesTag2 data decoding code https://github.com/crankyoldgit/IRremoteESP8266/pull/1380#discussion_r557357932
    Minor readability fixes

--- src/ir_MilesTag2.cpp
+++ src/ir_MilesTag2.cpp
@@ -1,4 +1,4 @@
-// Copyright 2020 Victor Mukayev (vitos1k)
+// Copyright 2021 Victor Mukayev (vitos1k)
 /// @file
 /// @brief Support for the MilesTag2 IR protocol for LaserTag gaming
 /// @see http://hosting.cmalton.me.uk/chrism/lasertag/MT2Proto.pdf
@@ -16,16 +16,21 @@
 #include "IRutils.h"
 
 // Constants
-const uint16_t kMilesHdrMark = 2400;
-const uint16_t kMilesSpace = 600;
-const uint16_t kMilesOneMark = 1200;
-const uint16_t kMilesZeroMark = 600;
+const uint16_t kMilesShotMask = 1 << (kMilesTag2ShotBits - 1); // Shot packets
+                                                               // have this bit
+                                                               // as `0`
+const uint32_t kMilesMsgMask = 1 << (kMilesTag2MsgBits - 1);  // Msg packets
+                                                              // have this bit
+                                                              // as `1`
+const uint8_t  kMilesMsgTerminator = 0xE8;
+const uint16_t kMilesHdrMark = 2400;  // Header
+const uint16_t kMilesSpace = 600;  // Space
+const uint16_t kMilesOneMark = 1200;  //One Mark
+const uint16_t kMilesZeroMark = 600;  //Zero Mark
 const uint16_t kMilesRptLength = 32000;
 const uint16_t kMilesMinGap = 32000;
 const uint16_t kMilesStdFreq = 38000;  // kHz
 const uint16_t kMilesStdDuty = 25;
-
-
 
 #if SEND_MILESTAG2
 /// Send a MilesTag2 formatted Shot packet.
@@ -33,7 +38,6 @@ const uint16_t kMilesStdDuty = 25;
 /// @param[in] data The message to be sent.
 /// @param[in] nbits The number of bits of message to be sent.
 /// @param[in] repeat The number of times the command is to be repeated.
-
 void IRsend::sendMilestag2(const uint64_t data, const uint16_t nbits,
                       const uint16_t repeat) {
   sendGeneric(
@@ -58,25 +62,11 @@ void IRsend::sendMilestag2(const uint64_t data, const uint16_t nbits,
 /// @see https://github.com/crankyoldgit/IRremoteESP8266/issues/706
 bool IRrecv::decodeMiles(decode_results *results, uint16_t offset,
                         const uint16_t nbits, const bool strict) {
-  /*
-  uint16_t gap_pos = 0;
-  // we got alot more data than we thought, 
-  // let's find last GAP and work from it
-  if (results->rawlen >= (2 * nbits + 1)) 
-  {
-      for (uint16_t ind = 0 ; ind < results->rawlen; ind++)
-      {
-          if (matchAtLeast(*(results->rawbuf+ind),34000)) gap_pos = ind;
-      }
-  }
-
-  if (results->rawlen>gap_pos) offset = gap_pos+1;
-  */ 
   // Compliance
   if (strict) {
     switch (nbits) {  // Check we've been called with a correct bit size.
-      case 14:
-      case 24:
+      case kMilesTag2ShotBits:
+      case kMilesTag2MsgBits:
         break;
       default:
         DPRINT("incorrect nbits:");
@@ -91,12 +81,31 @@ bool IRrecv::decodeMiles(decode_results *results, uint16_t offset,
                   kMilesOneMark, kMilesSpace,
                   kMilesZeroMark, kMilesSpace,
                   0, kMilesRptLength, true)) return false;
+  results->command = 0;
+  results->address = 0;
+
+  // Compliance
+  if (strict) {
+    switch (nbits) {
+      case kMilesTag2ShotBits:
+        // Is it a valid shot packet?
+        if (data & kMilesShotMask) return false;
+        results->command = data & 0x3F;  // Team & Damage
+        results->address = data >> 6;  // Player ID.
+        break;
+      case kMilesTag2MsgBits:
+        // Is it a valid msg packet? i.e. Msg bit set & Terminator present.
+        if (!(data & kMilesMsgMask) || ((data & 0xFF) != kMilesMsgTerminator))
+          return false;
+        results->command = (data >> 8) & 0xFF;  // Message data
+        results->address = (data >> 16) & 0x7F;  // Message ID
+        break;
+    }
+  }
   // Success
   results->bits = nbits;
   results->value = data;
   results->decode_type = decode_type_t::MILESTAG2;
-  results->command = 0;
-  results->address = 0;
   return true;
 }
 #endif  // DECODE_MILESTAG2
