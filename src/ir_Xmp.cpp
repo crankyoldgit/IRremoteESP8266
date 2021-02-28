da9acd4a11a203ce3c431ecd2e3cba44fa5284bc
crankyoldgit <david@xyzzy.com.au>
2021-02-28 17:13:44 +1000

    XMP: Update support for XMP repeat messages. * Correctly identify potential XMP repeat messages. * Use the correct values for an XMP message when sending a repeat. * Refactor the existing code. * Add unit tests that confirm the additions/changes.
    Fixes #1414

--- src/ir_Xmp.cpp
+++ src/ir_Xmp.cpp
@@ -24,23 +24,112 @@ const uint32_t kXmpMessageGap =  80400;  ///< uSeconds.
 const uint8_t  kXmpWordSize = kNibbleSize;  ///< nr. of Bits in a word.
 const uint8_t  kXmpMaxWordValue = (1 << kXmpWordSize) - 1;  // Max word value.
 const uint8_t  kXmpSections = 2;  ///< Nr. of Data sections
+const uint8_t  kXmpRepeatCode =    0b1000;
+const uint8_t  kXmpRepeatCodeAlt = 0b1001;
+
+using irutils::setBits;
+
+namespace IRXmpUtils {
+  /// Get the current checksum value from an XMP data section.
+  /// @param[in] data The value of the data section.
+  /// @param[in] nbits The number of data bits in the section.
+  /// @return The value of the stored checksum.
+  /// @warning Returns 0 if we can't obtain a valid checksum.
+  uint8_t getSectionChecksum(const uint32_t data, const uint16_t nbits) {
+    // The checksum is the 2nd most significant nibble of a section.
+    return (nbits < 2 * kNibbleSize) ? 0 : GETBITS32(data,
+                                                     nbits - (2 * kNibbleSize),
+                                                     kNibbleSize);
+  }
+
+  /// Calculate the correct checksum value for an XMP data section.
+  /// @param[in] data The value of the data section.
+  /// @param[in] nbits The number of data bits in the section.
+  /// @return The value of the correct checksum.
+  uint8_t calcSectionChecksum(const uint32_t data, const uint16_t nbits) {
+    return (0xF & ~(irutils::sumNibbles(data, nbits / kNibbleSize, 0xF, false) -
+                    getSectionChecksum(data, nbits)));
+  }
+
+  /// Recalculate a XMP message code ensuring it has the checksums valid.
+  /// @param[in] data The value of the XMP message code.
+  /// @param[in] nbits The number of data bits in the entire message code.
+  /// @return The corrected XMP message with valid checksum sections.
+  uint64_t updateChecksums(const uint64_t data, const uint16_t nbits) {
+    const uint16_t sectionbits = nbits / kXmpSections;
+    uint64_t result = data;
+    for (uint16_t sectionOffset = 0; sectionOffset < nbits;
+         sectionOffset += sectionbits) {
+      const uint16_t checksumOffset = sectionOffset + sectionbits -
+                                      (2 * kNibbleSize);
+      setBits(&result, checksumOffset, kNibbleSize,
+              calcSectionChecksum(GETBITS64(data, sectionOffset, sectionbits),
+                                  sectionbits));
+    }
+    return result;
+  }
+
+  /// Calculate the bit offset the repeat nibble in an XMP code.
+  /// @param[in] nbits The number of data bits in the entire message code.
+  /// @return The offset to the start of the XMP repeat nibble.
+  uint16_t calcRepeatOffset(const uint16_t nbits) {
+    return (nbits < 3 * kNibbleSize) ? 0
+                                     : (nbits / kXmpSections) -
+                                       (3 * kNibbleSize);
+  }
+
+  /// Test if an XMP message code is a repeat or not.
+  /// @param[in] data The value of the XMP message code.
+  /// @param[in] nbits The number of data bits in the entire message code.
+  /// @return true, if it looks like a repeat, false if not.
+  bool isRepeat(const uint64_t data, const uint16_t nbits) {
+    switch (GETBITS64(data, calcRepeatOffset(nbits), kNibbleSize)) {
+      case kXmpRepeatCode:
+      case kXmpRepeatCodeAlt:
+        return true;
+      default:
+        return false;
+    }
+  }
+
+  /// Adjust an XMP message code to make it a valid repeat or non-repeat code.
+  /// @param[in] data The value of the XMP message code.
+  /// @param[in] nbits The number of data bits in the entire message code.
+  /// @param[in] repeat_code The value of the XMP repeat nibble to use.
+  ///   A value of `8` is the normal value for a repeat. `9` has also been seen.
+  ///   A value of `0` will convert the code to a non-repeat code.
+  /// @return The valud of the modified XMP code.
+  uint64_t adjustRepeat(const uint64_t data, const uint16_t nbits,
+                        const uint8_t repeat_code) {
+    uint64_t result = data;
+    setBits(&result, calcRepeatOffset(nbits), kNibbleSize, repeat_code);
+    return updateChecksums(result, nbits);
+  }
+}  // namespace IRXmpUtils
+
+using IRXmpUtils::calcSectionChecksum;
+using IRXmpUtils::getSectionChecksum;
+using IRXmpUtils::isRepeat;
+using IRXmpUtils::adjustRepeat;
 
 
 #if SEND_XMP
 /// Send a XMP packet.
-/// Status:  ALPHA / Untested.
+/// Status:  Beta / Untested against a real device.
 /// @param[in] data The message to be sent.
 /// @param[in] nbits The number of bits of message to be sent.
 /// @param[in] repeat The number of times the command is to be repeated.
-void IRsend::sendXmp(uint64_t data, uint16_t nbits, uint16_t repeat) {
+void IRsend::sendXmp(const uint64_t data, const uint16_t nbits,
+                     const uint16_t repeat) {
   enableIROut(38000);
   if (nbits < 2 * kXmpWordSize) return;  // Too small to send, abort!
+  uint64_t send_data = data;
   for (uint16_t r = 0; r <= repeat; r++) {
     uint16_t bits_so_far = kXmpWordSize;
     for (uint64_t mask = ((uint64_t)kXmpMaxWordValue) << (nbits - kXmpWordSize);
          mask;
          mask >>= kXmpWordSize) {
-      uint8_t word = (data & mask) >> (nbits - bits_so_far);
+      uint8_t word = (send_data & mask) >> (nbits - bits_so_far);
       mark(kXmpMark);
       space(kXmpBaseSpace + word * kXmpSpaceStep);
       bits_so_far += kXmpWordSize;
@@ -51,6 +140,10 @@ void IRsend::sendXmp(uint64_t data, uint16_t nbits, uint16_t repeat) {
       }
     }
     space(kXmpMessageGap - kXmpFooterSpace);
+
+    // Modify the value if needed, to make it into a valid repeat code.
+    if (!isRepeat(send_data, nbits))
+      send_data = adjustRepeat(send_data, nbits, kXmpRepeatCode);
   }
 }
 #endif  // SEND_XMP
@@ -113,13 +206,8 @@ bool IRrecv::decodeXmp(decode_results *results, uint16_t offset,
     const uint16_t section_size = nbits / kXmpSections;
     // Each section has a checksum.
     for (uint16_t section = 0; section < kXmpSections; section++) {
-      // The checksum is the 2nd most significant nibble of a section.
-      const uint8_t checksum = GETBITS64(checksum_data,
-                                         section_size - (2 * kNibbleSize),
-                                         kNibbleSize);
-      if (checksum != (0xF & ~(irutils::sumNibbles(checksum_data,
-                                                   section_size / kNibbleSize,
-                                                   0xF, false) - checksum)))
+      if (getSectionChecksum(checksum_data, section_size) !=
+          calcSectionChecksum(checksum_data, section_size))
         return 0;
       checksum_data >>= section_size;
     }
@@ -131,6 +219,8 @@ bool IRrecv::decodeXmp(decode_results *results, uint16_t offset,
   results->bits = nbits;
   results->address = 0;
   results->command = 0;
+  // See if it is a repeat message.
+  results->repeat = isRepeat(data, nbits);
   return true;
 }
 #endif  // DECODE_XMP
--- test/ir_Xmp_test.cpp
+++ test/ir_Xmp_test.cpp
@@ -68,6 +68,67 @@ TEST(TestDecodeXmp, RealMessageDecode) {
   EXPECT_FALSE(irsend.capture.repeat);
 }
 
+// Decode a repeat of a Xmp message.
+TEST(TestDecodeXmp, RepeatMessageDecode) {
+  IRsendTest irsend(kGpioUnused);
+  IRrecv irrecv(kGpioUnused);
+  irsend.begin();
+
+  // Real-life XMP code from an actual capture/decode. (Down, repeat)
+  // Ref: https://github.com/crankyoldgit/IRremoteESP8266/issues/1414#issuecomment-787203548
+  irsend.reset();
+  irsend.sendXmp(0x170F443E14802100);
+  irsend.makeDecodeResult();
+  EXPECT_TRUE(irrecv.decode(&irsend.capture));
+  EXPECT_EQ(decode_type_t::XMP, irsend.capture.decode_type);
+  EXPECT_EQ(kXmpBits, irsend.capture.bits);
+  EXPECT_EQ(0x170F443E14802100, irsend.capture.value);
+  EXPECT_EQ(0x0, irsend.capture.address);
+  EXPECT_EQ(0x0, irsend.capture.command);
+  EXPECT_TRUE(irsend.capture.repeat);
+}
+
+// Decode a repeat of a Xmp message.
+TEST(TestSendXmp, SendDataWithRepeat) {
+  IRsendTest irsend(kGpioUnused);
+  IRrecv irrecv(kGpioUnused);
+  irsend.begin();
+
+  irsend.reset();
+  irsend.sendXmp(0x170F443E1B002200, kXmpBits, 1);  // Down + repeat
+  irsend.makeDecodeResult();
+
+  // Normal (non-repeat) code.
+  EXPECT_TRUE(irrecv.decode(&irsend.capture));
+  EXPECT_EQ(decode_type_t::XMP, irsend.capture.decode_type);
+  EXPECT_EQ(kXmpBits, irsend.capture.bits);
+  EXPECT_EQ(0x170F443E1B002200, irsend.capture.value);
+  EXPECT_EQ(0x0, irsend.capture.address);
+  EXPECT_EQ(0x0, irsend.capture.command);
+  EXPECT_FALSE(irsend.capture.repeat);
+
+  // Now the repeated (altered) code which is after the "normal" code.
+  EXPECT_TRUE(irrecv.decodeXmp(&irsend.capture, kStartOffset + 36));
+  EXPECT_EQ(decode_type_t::XMP, irsend.capture.decode_type);
+  EXPECT_EQ(kXmpBits, irsend.capture.bits);
+  EXPECT_EQ(0x170F443E13802200, irsend.capture.value);
+  EXPECT_EQ(0x0, irsend.capture.address);
+  EXPECT_EQ(0x0, irsend.capture.command);
+  EXPECT_TRUE(irsend.capture.repeat);
+
+  EXPECT_EQ(
+      "f38000d50"
+      "m210s895m210s1705m210s760m210s2785m210s1300m210s1300m210s1165m210s2650"
+      "m210s13000"
+      "m210s895m210s2245m210s760m210s760m210s1030m210s1030m210s760m210s760"
+      "m210s80400"
+      "m210s895m210s1705m210s760m210s2785m210s1300m210s1300m210s1165m210s2650"
+      "m210s13000"
+      "m210s895m210s1165m210s1840m210s760m210s1030m210s1030m210s760m210s760"
+      "m210s80400",
+      irsend.outputStr());
+}
+
 TEST(TestUtils, Housekeeping) {
   ASSERT_EQ("XMP", typeToString(decode_type_t::XMP));
   ASSERT_EQ(decode_type_t::XMP, strToDecodeType("XMP"));
