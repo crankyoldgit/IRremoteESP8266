4fedb9e9e8f649a6b616fa121104d78e4a3d2ba5
crankyoldgit <david@xyzzy.com.au>
2021-06-01 21:26:34 +1000

    Try to make warnings not cause an error when no protocols are selected.

--- src/IRac.cpp
+++ src/IRac.cpp
@@ -293,6 +293,7 @@ bool IRac::isProtocolSupported(const decode_type_t protocol) {
 //       "-Wswitch-unreachable" not used as it appears to be an unknown option.
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wall"
+#pragma GCC diagnostic ignored "-Werror=switch-unreachable"
       return true;
 #pragma GCC diagnostic pop
     default:
