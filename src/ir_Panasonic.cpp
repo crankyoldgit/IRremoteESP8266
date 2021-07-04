crankyoldgit/IRremoteESP8266

commit 95a2563a1440bdb2427dab9f4c47d1091a9b23c5

Refactor to use common routines/macros to handle bit manipulation. (#934)
* Add `setBit()`, `setBits()` methods to simplify & standardise bit manipulation.
  o Unit tests for above.
* Add macros for `GETBIT8()` -> `GETBIT64()` & `GETBITS8()` -> `GETBITS64()`for standardising bit retrieval.
  o Macro used instead of a function as the overhead was too expensive. i.e. program space got bigger in most cases.
* Improved bit handling for some protocols.
  o Changes the library values of some settings in a few protocols.
  o This required some minor changes to Unit Tests to match new values.
* Refactor all the A/C protocols to use the above.
 o Above routines covered 99+% of use cases.

* Passes all the existing unit tests without modification (see above for exceptions) so this is pretty much safe and should not affect functionality.
* Fixes a number of potential issues where poor bit-wise math was used previously.
* Program size remains roughly the same.
  o Some gains and some losses. Overall it's worth it.
* Code style/formatting changes & improvements.

src/ir_Panasonic.h
@@ -42,40 +42,45 @@ const uint16_t kPanasonicAcExcess = 0;
 // Much higher than usual. See issue #540.
 const uint16_t kPanasonicAcTolerance = 40;
 
-const uint8_t kPanasonicAcAuto = 0;  // 0b0000
-const uint8_t kPanasonicAcDry = 2;   // 0b0010
-const uint8_t kPanasonicAcCool = 3;  // 0b0011
-const uint8_t kPanasonicAcHeat = 4;  // 0b0010
-const uint8_t kPanasonicAcFan = 6;   // 0b0110
+const uint8_t kPanasonicAcAuto = 0;  // 0b000
+const uint8_t kPanasonicAcDry = 2;   // 0b010
+const uint8_t kPanasonicAcCool = 3;  // 0b011
+const uint8_t kPanasonicAcHeat = 4;  // 0b010
+const uint8_t kPanasonicAcFan = 6;   // 0b110
 const uint8_t kPanasonicAcFanMin = 0;
 const uint8_t kPanasonicAcFanMed = 2;
 const uint8_t kPanasonicAcFanMax = 4;
 const uint8_t kPanasonicAcFanAuto = 7;
-const uint8_t kPanasonicAcFanOffset = 3;
-const uint8_t kPanasonicAcPower = 1;         // 0b1
+const uint8_t kPanasonicAcFanDelta = 3;
+const uint8_t kPanasonicAcPowerOffset = 0;
+const uint8_t kPanasonicAcTempOffset = 1;  // Bits
+const uint8_t kPanasonicAcTempSize = 5;  // Bits
 const uint8_t kPanasonicAcMinTemp = 16;      // Celsius
 const uint8_t kPanasonicAcMaxTemp = 30;      // Celsius
 const uint8_t kPanasonicAcFanModeTemp = 27;  // Celsius
-const uint8_t kPanasonicAcQuiet = 1;         // 0b1
-const uint8_t kPanasonicAcPowerful = 0x20;   // 0b100000
+const uint8_t kPanasonicAcQuietOffset = 0;
+const uint8_t kPanasonicAcPowerfulOffset = 5;   // 0b100000
 // CKP & RKR models have Powerful and Quiet bits swapped.
-const uint8_t kPanasonicAcQuietCkp = 0x20;  // 0b100000
-const uint8_t kPanasonicAcPowerfulCkp = 1;  // 0b1
-const uint8_t kPanasonicAcSwingVAuto = 0xF;
-const uint8_t kPanasonicAcSwingVHighest = 0x1;
-const uint8_t kPanasonicAcSwingVHigh = 0x2;
-const uint8_t kPanasonicAcSwingVMiddle = 0x3;
-const uint8_t kPanasonicAcSwingVLow = 0x4;
-const uint8_t kPanasonicAcSwingVLowest = 0x5;
-const uint8_t kPanasonicAcSwingHAuto = 0xD;
-const uint8_t kPanasonicAcSwingHMiddle = 0x6;
-const uint8_t kPanasonicAcSwingHFullLeft = 0x9;
-const uint8_t kPanasonicAcSwingHLeft = 0xA;
-const uint8_t kPanasonicAcSwingHRight = 0xB;
-const uint8_t kPanasonicAcSwingHFullRight = 0xC;
+const uint8_t kPanasonicAcQuietCkpOffset = kPanasonicAcPowerfulOffset;
+const uint8_t kPanasonicAcPowerfulCkpOffset = kPanasonicAcQuietOffset;
+const uint8_t kPanasonicAcSwingVHighest = 0x1;  // 0b0001
+const uint8_t kPanasonicAcSwingVHigh = 0x2;     // 0b0010
+const uint8_t kPanasonicAcSwingVMiddle = 0x3;   // 0b0011
+const uint8_t kPanasonicAcSwingVLow = 0x4;      // 0b0100
+const uint8_t kPanasonicAcSwingVLowest = 0x5;   // 0b0101
+const uint8_t kPanasonicAcSwingVAuto = 0xF;     // 0b1111
+
+const uint8_t kPanasonicAcSwingHMiddle = 0x6;     // 0b0110
+const uint8_t kPanasonicAcSwingHFullLeft = 0x9;   // 0b1001
+const uint8_t kPanasonicAcSwingHLeft = 0xA;       // 0b1010
+const uint8_t kPanasonicAcSwingHRight = 0xB;      // 0b1011
+const uint8_t kPanasonicAcSwingHFullRight = 0xC;  // 0b1100
+const uint8_t kPanasonicAcSwingHAuto = 0xD;       // 0b1101
 const uint8_t kPanasonicAcChecksumInit = 0xF4;
-const uint8_t kPanasonicAcOnTimer = 0b00000010;
-const uint8_t kPanasonicAcOffTimer = 0b00000100;
+const uint8_t kPanasonicAcOnTimerOffset = 1;
+const uint8_t kPanasonicAcOffTimerOffset = 2;
+const uint8_t kPanasonicAcTimeSize = 11;  // Bits
+const uint8_t kPanasonicAcTimeOverflowSize = 3;  // Bits
 const uint16_t kPanasonicAcTimeMax = 23 * 60 + 59;  // Mins since midnight.
 const uint16_t kPanasonicAcTimeSpecial = 0x600;
 
@@ -157,6 +162,9 @@ class IRPanasonicAc {
   void fixChecksum(const uint16_t length = kPanasonicAcStateLength);
   static uint8_t calcChecksum(const uint8_t *state,
                               const uint16_t length = kPanasonicAcStateLength);
+  static uint16_t _getTime(const uint8_t ptr[]);
+  static void _setTime(uint8_t * const ptr, const uint16_t mins_since_midnight,
+                       const bool round_down);
 };
 
 #endif  // IR_PANASONIC_H_

test/IRac_test.cpp
@@ -494,8 +494,8 @@ TEST(TestIRac, HaierYrwo2) {
   IRac irac(0);
   IRrecv capture(0);
   char expected[] =
-      "Power: On, Button: 5 (Power), Mode: 2 (Cool), Temp: 23C, "
-      "Fan: 4 (Medium), Turbo: 1 (High), Swing: 1 (Highest), Sleep: On, "
+      "Power: On, Button: 5 (Power), Mode: 1 (Cool), Temp: 23C, "
+      "Fan: 2 (Medium), Turbo: 1 (High), Swing: 1 (Highest), Sleep: On, "
       "Health: On";
 
   ac.begin();
@@ -603,7 +603,7 @@ TEST(TestIRac, Mitsubishi) {
   IRac irac(0);
   IRrecv capture(0);
   char expected[] =
-      "Power: On, Mode: 24 (Cool), Temp: 20C, Fan: 2 (Medium), "
+      "Power: On, Mode: 3 (Cool), Temp: 20C, Fan: 2 (Medium), "
       "Swing(V): 0 (Auto), Swing(H): 3 (UNKNOWN), "
       "Clock: 14:30, On Timer: 00:00, Off Timer: 00:00, Timer: -";
 
@@ -655,7 +655,7 @@ TEST(TestIRac, MitsubishiHeavy88) {
   IRrecv capture(0);
   char expected[] =
       "Power: On, Mode: 1 (Cool), Temp: 21C, Fan: 3 (Med), "
-      "Swing(V): 16 (Auto), Swing(H): 0 (Off), Turbo: Off, Econo: Off, "
+      "Swing(V): 4 (Auto), Swing(H): 0 (Off), Turbo: Off, Econo: Off, "
       "3D: Off, Clean: On";
 
   ac.begin();

test/ir_Panasonic_test.cpp
@@ -647,15 +647,17 @@ TEST(TestIRPanasonicAcClass, SetAndGetFan) {
   pana.setFan(kPanasonicAcFanMin);
   EXPECT_EQ(kPanasonicAcFanMin, pana.getFan());
   pana.setFan(kPanasonicAcFanMin - 1);
-  EXPECT_EQ(kPanasonicAcFanMin, pana.getFan());
+  EXPECT_EQ(kPanasonicAcFanAuto, pana.getFan());
+  pana.setFan(kPanasonicAcFanMed);
+  EXPECT_EQ(kPanasonicAcFanMed, pana.getFan());
   pana.setFan(kPanasonicAcFanMin + 1);
-  EXPECT_EQ(kPanasonicAcFanMin + 1, pana.getFan());
+  EXPECT_EQ(kPanasonicAcFanAuto, pana.getFan());
   pana.setFan(kPanasonicAcFanMax);
   EXPECT_EQ(kPanasonicAcFanMax, pana.getFan());
   pana.setFan(kPanasonicAcFanMax + 1);
-  EXPECT_EQ(kPanasonicAcFanMax, pana.getFan());
+  EXPECT_EQ(kPanasonicAcFanAuto, pana.getFan());
   pana.setFan(kPanasonicAcFanMax - 1);
-  EXPECT_EQ(kPanasonicAcFanMax - 1, pana.getFan());
+  EXPECT_EQ(kPanasonicAcFanAuto, pana.getFan());
 }
 
 TEST(TestIRPanasonicAcClass, SetAndGetSwings) {

src/IRutils.cpp
@@ -1033,4 +1033,152 @@ namespace irutils {
     if (integer > 99) return 255;  // Too big.
     return ((integer / 10) << 4) + (integer % 10);
   }
+
+  // Return the value of `position`th bit of `data`.
+  // Args:
+  //   data: Value to be examined.
+  //   position: Nr. of the nth bit to be examined. `0` is the LSB.
+  //   size: Nr. of bits in data.
+  bool getBit(const uint64_t data, const uint8_t position, const uint8_t size) {
+    if (position >= size) return false;  // Outside of range.
+    return data & (1ULL << position);
+  }
+
+  // Return the value of `position`th bit of `data`.
+  // Args:
+  //   data: Value to be examined.
+  //   position: Nr. of the nth bit to be examined. `0` is the LSB.
+  bool getBit(const uint8_t data, const uint8_t position) {
+    if (position >= 8) return false;  // Outside of range.
+    return data & (1 << position);
+  }
+
+  // Return the value of `data` with the `position`th bit changed to `on`
+  // Args:
+  //   data: Value to be changed.
+  //   position: Nr. of the bit to be changed. `0` is the LSB.
+  //   on: Value to set the position'th bit to.
+  //   size: Nr. of bits in data.
+  uint64_t setBit(const uint64_t data, const uint8_t position, const bool on,
+                  const uint8_t size) {
+    if (position >= size) return data;  // Outside of range.
+    uint64_t mask = 1ULL << position;
+    if (on)
+      return data | mask;
+    else
+      return data & ~mask;
+  }
+
+  // Return the value of `data` with the `position`th bit changed to `on`
+  // Args:
+  //   data: Value to be changed.
+  //   position: Nr. of the bit to be changed. `0` is the LSB.
+  //   on: Value to set the position'th bit to.
+  uint8_t setBit(const uint8_t data, const uint8_t position, const bool on) {
+    if (position >= 8) return data;  // Outside of range.
+    uint8_t mask = 1 << position;
+    if (on)
+      return data | mask;
+    else
+      return data & ~mask;
+  }
+
+  // Change the value at the location `data_ptr` with the `position`th bit
+  //   changed to `on`
+  // Args:
+  //   data: Ptr to the data to be changed.
+  //   position: Nr. of the bit to be changed. `0` is the LSB.
+  //   on: Value to set the position'th bit to.
+  void setBit(uint8_t * const data, const uint8_t position, const bool on) {
+    uint8_t mask = 1 << position;
+    if (on)
+      *data |= mask;
+    else
+      *data &= ~mask;
+  }
+
+  // Change the value at the location `data_ptr` with the `position`th bit
+  //   changed to `on`
+  // Args:
+  //   data: Ptr to the data to be changed.
+  //   position: Nr. of the bit to be changed. `0` is the LSB.
+  //   on: Value to set the position'th bit to.
+  void setBit(uint32_t * const data, const uint8_t position, const bool on) {
+    uint32_t mask = (uint32_t)1 << position;
+    if (on)
+      *data |= mask;
+    else
+      *data &= ~mask;
+  }
+
+  // Change the value at the location `data_ptr` with the `position`th bit
+  //   changed to `on`
+  // Args:
+  //   data: Ptr to the data to be changed.
+  //   position: Nr. of the bit to be changed. `0` is the LSB.
+  //   on: Value to set the position'th bit to.
+  void setBit(uint64_t * const data, const uint8_t position, const bool on) {
+    uint64_t mask = (uint64_t)1 << position;
+    if (on)
+      *data |= mask;
+    else
+      *data &= ~mask;
+  }
+
+  // Change the uint8_t pointed to by `dst` starting at the `offset`th bit
+  //   and for `nbits` bits, with the contents of `data`.
+  // Args:
+  //   dst: Ptr to the uint8_t to be changed.
+  //   offset: Nr. of bits from the Least Significant Bit to be ignored.
+  //   nbits: Nr of bits of `data` to be placed into the destination uint8_t.
+  //   data: Value to be placed into dst.
+  void setBits(uint8_t * const dst, const uint8_t offset, const uint8_t nbits,
+               const uint8_t data) {
+    if (offset >= 8 || !nbits) return;  // Short circuit as it won't change.
+    // Calculate the mask for the supplied value.
+    uint8_t mask = UINT8_MAX >> (8 - ((nbits > 8) ? 8 : nbits));
+    // Calculate the mask & clear the space for the data.
+    // Clear the destination bits.
+    *dst &= ~(uint8_t)(mask << offset);
+    // Merge in the data.
+    *dst |= ((data & mask) << offset);
+  }
+
+  // Change the uint32_t pointed to by `dst` starting at the `offset`th bit
+  //   and for `nbits` bits, with the contents of `data`.
+  // Args:
+  //   dst: Ptr to the uint32_t to be changed.
+  //   offset: Nr. of bits from the Least Significant Bit to be ignored.
+  //   nbits: Nr of bits of `data` to be placed into the destination uint32_t.
+  //   data: Value to be placed into dst.
+  void setBits(uint32_t * const dst, const uint8_t offset, const uint8_t nbits,
+               const uint32_t data) {
+    if (offset >= 32 || !nbits) return;  // Short circuit as it won't change.
+    // Calculate the mask for the supplied value.
+    uint32_t mask = UINT32_MAX >> (32 - ((nbits > 32) ? 32 : nbits));
+    // Calculate the mask & clear the space for the data.
+    // Clear the destination bits.
+    *dst &= ~(mask << offset);
+    // Merge in the data.
+    *dst |= ((data & mask) << offset);
+  }
+
+  // Change the uint64_t pointed to by `dst` starting at the `offset`th bit
+  //   and for `nbits` bits, with the contents of `data`.
+  // Args:
+  //   dst: Ptr to the uint64_t to be changed.
+  //   offset: Nr. of bits from the Least Significant Bit to be ignored.
+  //   nbits: Nr of bits of `data` to be placed into the destination uint64_t.
+  //   data: Value to be placed into dst.
+  void setBits(uint64_t * const dst, const uint8_t offset, const uint8_t nbits,
+               const uint64_t data) {
+    if (offset >= 64 || !nbits) return;  // Short circuit as it won't change.
+    // Calculate the mask for the supplied value.
+    uint64_t mask = UINT64_MAX >> (64 - ((nbits > 64) ? 64 : nbits));
+    // Calculate the mask & clear the space for the data.
+    // Clear the destination bits.
+    *dst &= ~(mask << offset);
+    // Merge in the data.
+    *dst |= ((data & mask) << offset);
+  }
 }  // namespace irutils

src/IRutils.h
@@ -14,6 +14,10 @@
 #include "IRremoteESP8266.h"
 #include "IRrecv.h"
 
+const uint8_t kNibbleSize = 4;
+const uint8_t kLowNibble = 0;
+const uint8_t kHighNibble = 4;
+const uint8_t kModeBitsSize = 3;
 uint64_t reverseBits(uint64_t input, uint16_t nbits);
 String uint64ToString(uint64_t input, uint8_t base = 10);
 String typeToString(const decode_type_t protocol,
@@ -65,5 +69,39 @@ namespace irutils {
                      const uint8_t init = 0);
   uint8_t bcdToUint8(const uint8_t bcd);
   uint8_t uint8ToBcd(const uint8_t integer);
+  bool getBit(const uint64_t data, const uint8_t position,
+              const uint8_t size = 64);
+  bool getBit(const uint8_t data, const uint8_t position);
+#define GETBIT8(a, b) (a & ((uint8_t)1 << b))
+#define GETBIT16(a, b) (a & ((uint16_t)1 << b))
+#define GETBIT32(a, b) (a & ((uint32_t)1 << b))
+#define GETBIT64(a, b) (a & ((uint64_t)1 << b))
+#define GETBITS8(data, offset, size) \
+    (((data) & (((uint8_t)UINT8_MAX >> (8 - (size))) << (offset))) >> (offset))
+#define GETBITS16(data, offset, size) \
+    (((data) & (((uint16_t)UINT16_MAX >> (16 - (size))) << (offset))) >> \
+     (offset))
+#define GETBITS32(data, offset, size) \
+    (((data) & (((uint32_t)UINT32_MAX >> (32 - (size))) << (offset))) >> \
+     (offset))
+#define GETBITS64(data, offset, size) \
+    (((data) & (((uint64_t)UINT64_MAX >> (64 - (size))) << (offset))) >> \
+     (offset))
+  uint64_t setBit(const uint64_t data, const uint8_t position,
+                  const bool on = true, const uint8_t size = 64);
+  uint8_t setBit(const uint8_t data, const uint8_t position,
+                 const bool on = true);
+  void setBit(uint8_t * const data, const uint8_t position,
+              const bool on = true);
+  void setBit(uint32_t * const data, const uint8_t position,
+              const bool on = true);
+  void setBit(uint64_t * const data, const uint8_t position,
+              const bool on = true);
+  void setBits(uint8_t * const dst, const uint8_t offset, const uint8_t nbits,
+               const uint8_t data);
+  void setBits(uint32_t * const dst, const uint8_t offset, const uint8_t nbits,
+               const uint32_t data);
+  void setBits(uint64_t * const dst, const uint8_t offset, const uint8_t nbits,
+               const uint64_t data);
 }  // namespace irutils
 #endif  // IRUTILS_H_

src/ir_Amcor.cpp
@@ -29,6 +29,7 @@ using irutils::addBoolToString;
 using irutils::addModeToString;
 using irutils::addFanToString;
 using irutils::addTempToString;
+using irutils::setBits;
 
 #if SEND_AMCOR
 // Send a Amcor HVAC formatted message.
@@ -145,49 +146,45 @@ void IRAmcorAc::on(void) { setPower(true); }
 void IRAmcorAc::off(void) { setPower(false); }
 
 void IRAmcorAc::setPower(const bool on) {
-  remote_state[kAmcorPowerByte] &= ~kAmcorPowerMask;
-  remote_state[kAmcorPowerByte] |= (on ? kAmcorPowerOn : kAmcorPowerOff);
+  setBits(&remote_state[kAmcorPowerByte], kAmcorPowerOffset, kAmcorPowerSize,
+          on ? kAmcorPowerOn : kAmcorPowerOff);
 }
 
 bool IRAmcorAc::getPower(void) {
-  return ((remote_state[kAmcorPowerByte] & kAmcorPowerMask) == kAmcorPowerOn);
+  return GETBITS8(remote_state[kAmcorPowerByte], kAmcorPowerOffset,
+                  kAmcorPowerSize) == kAmcorPowerOn;
 }
 
 // Set the temp in deg C
 void IRAmcorAc::setTemp(const uint8_t degrees) {
   uint8_t temp = std::max(kAmcorMinTemp, degrees);
   temp = std::min(kAmcorMaxTemp, temp);
-
-  temp <<= 1;
-  remote_state[kAmcorTempByte] &= ~kAmcorTempMask;
-  remote_state[kAmcorTempByte] |= temp;
+  setBits(&remote_state[kAmcorTempByte], kAmcorTempOffset, kAmcorTempSize,
+          temp);
 }
 
 uint8_t IRAmcorAc::getTemp(void) {
-  return (remote_state[kAmcorTempByte] & kAmcorTempMask) >> 1;
+  return GETBITS8(remote_state[kAmcorTempByte], kAmcorTempOffset,
+                  kAmcorTempSize);
 }
 
 // Maximum Cooling or Hearing
 void IRAmcorAc::setMax(const bool on) {
   if (on) {
     switch (getMode()) {
-      case kAmcorCool:
-        setTemp(kAmcorMinTemp);
-        break;
-      case kAmcorHeat:
-        setTemp(kAmcorMaxTemp);
-        break;
-      default:  // Not allowed in all other operating modes.
-        return;
+      case kAmcorCool: setTemp(kAmcorMinTemp); break;
+      case kAmcorHeat: setTemp(kAmcorMaxTemp); break;
+      // Not allowed in all other operating modes.
+      default: return;
     }
-    remote_state[kAmcorSpecialByte] |= kAmcorMaxMask;
-  } else {
-    remote_state[kAmcorSpecialByte] &= ~kAmcorMaxMask;
   }
+  setBits(&remote_state[kAmcorSpecialByte], kAmcorMaxOffset, kAmcorMaxSize,
+          on ? kAmcorMax : 0);
 }
 
 bool IRAmcorAc::getMax(void) {
-  return ((remote_state[kAmcorSpecialByte] & kAmcorMaxMask) == kAmcorMaxMask);
+  return GETBITS8(remote_state[kAmcorSpecialByte], kAmcorMaxOffset,
+                  kAmcorMaxSize) == kAmcorMax;
 }
 
 // Set the speed of the fan
@@ -197,8 +194,8 @@ void IRAmcorAc::setFan(const uint8_t speed) {
     case kAmcorFanMin:
     case kAmcorFanMed:
     case kAmcorFanMax:
-      remote_state[kAmcorModeFanByte] &= ~kAmcorFanMask;
-      remote_state[kAmcorModeFanByte] |= speed << 4;
+      setBits(&remote_state[kAmcorModeFanByte], kAmcorFanOffset, kAmcorFanSize,
+              speed);
       break;
     default:
       setFan(kAmcorFanAuto);
@@ -206,27 +203,26 @@ void IRAmcorAc::setFan(const uint8_t speed) {
 }
 
 uint8_t IRAmcorAc::getFan(void) {
-  return (remote_state[kAmcorModeFanByte] & kAmcorFanMask) >> 4;
+  return GETBITS8(remote_state[kAmcorModeFanByte], kAmcorFanOffset,
+                  kAmcorFanSize);
 }
 
 uint8_t IRAmcorAc::getMode(void) {
-  return remote_state[kAmcorModeFanByte] & kAmcorModeMask;
+  return GETBITS8(remote_state[kAmcorModeFanByte], kAmcorModeOffset,
+                  kAmcorModeSize);
 }
 
 void IRAmcorAc::setMode(const uint8_t mode) {
-  remote_state[kAmcorSpecialByte] &= ~kAmcorVentMask;  // Clear the vent setting
   switch (mode) {
     case kAmcorFan:
-      remote_state[kAmcorSpecialByte] |= kAmcorVentMask;  // Set the vent option
-      // FALL-THRU
     case kAmcorCool:
     case kAmcorHeat:
     case kAmcorDry:
     case kAmcorAuto:
-      // Mask out bits
-      remote_state[kAmcorModeFanByte] &= ~kAmcorModeMask;
-      // Set the mode at bit positions
-      remote_state[kAmcorModeFanByte] |= mode;
+      setBits(&remote_state[kAmcorSpecialByte], kAmcorVentOffset,
+              kAmcorVentSize, (mode == kAmcorFan) ? kAmcorVentOn : 0);
+      setBits(&remote_state[kAmcorModeFanByte], kAmcorModeOffset,
+              kAmcorModeSize, mode);
       return;
     default:
       this->setMode(kAmcorAuto);

src/ir_Amcor.h
@@ -35,36 +35,44 @@ const uint8_t kAmcorFanMin =   0b001;
 const uint8_t kAmcorFanMed =   0b010;
 const uint8_t kAmcorFanMax =   0b011;
 const uint8_t kAmcorFanAuto =  0b100;
-const uint8_t kAmcorFanMask = 0b01110000;
+const uint8_t kAmcorFanOffset = 4;
+const uint8_t kAmcorFanSize = 3;
 // Modes
 const uint8_t kAmcorCool =          0b001;
 const uint8_t kAmcorHeat =          0b010;
 const uint8_t kAmcorFan =           0b011;  // Aka "Vent"
 const uint8_t kAmcorDry =           0b100;
 const uint8_t kAmcorAuto =          0b101;
-const uint8_t kAmcorModeMask = 0b00000111;
+const uint8_t kAmcorModeOffset = 0;
+const uint8_t kAmcorModeSize = 3;
 
 // state[2]
 const uint8_t kAmcorTempByte = 2;
 // Temperature
 const uint8_t kAmcorMinTemp = 12;  // Celsius
 const uint8_t kAmcorMaxTemp = 32;  // Celsius
-const uint8_t kAmcorTempMask = 0b01111110;
+const uint8_t kAmcorTempOffset = 1;
+const uint8_t kAmcorTempSize = 6;  // Bits
 
 // state[5]
 // Power
 const uint8_t kAmcorPowerByte = 5;
-const uint8_t kAmcorPowerMask = 0b11110000;
-const uint8_t kAmcorPowerOn =   0b00110000;  // 0x30
-const uint8_t kAmcorPowerOff =  0b11000000;  // 0xC0
+const uint8_t kAmcorPowerOffset = 4;
+const uint8_t kAmcorPowerSize = 4;
+const uint8_t kAmcorPowerOn =   0b0011;  // 0x3
+const uint8_t kAmcorPowerOff =  0b1100;  // 0xC
 
 // state[6]
 const uint8_t kAmcorSpecialByte = 6;
 // Max Mode (aka "Lo" in Cool and "Hi" in Heat)
-const uint8_t kAmcorMaxMask =    0b00000011;  // 0x03
-// "Vent" Mode
-const uint8_t kAmcorVentMask =  0b11000000;  // 0xC0
+const uint8_t kAmcorMax = 0b11;
+const uint8_t kAmcorMaxOffset = 0;
+const uint8_t kAmcorMaxSize = 2;
 
+// "Vent" Mode
+const uint8_t kAmcorVentOn =  0b11;
+const uint8_t kAmcorVentOffset = 6;
+const uint8_t kAmcorVentSize = 2;
 // state[7]
 // Checksum byte.
 const uint8_t kAmcorChecksumByte = kAmcorStateLength - 1;

commit 3c1862f1ad1ad4f92fbe56a370c269d5553ca275

Panasonic AC - Fix Low and High fan speeds (#1515)
* Add Fan low and high speeds to setFan
* Adjust unit tests accordingly.
* Fixes a regression introduced in #934 

Co-authored-by: David Conran <crankyoldgit@users.noreply.github.com>

src/ir_Panasonic.cpp
@@ -455,7 +455,9 @@ void IRPanasonicAc::setSwingHorizontal(const uint8_t desired_direction) {
 void IRPanasonicAc::setFan(const uint8_t speed) {
   switch (speed) {
     case kPanasonicAcFanMin:
+    case kPanasonicAcFanLow:
     case kPanasonicAcFanMed:
+    case kPanasonicAcFanHigh:
     case kPanasonicAcFanMax:
     case kPanasonicAcFanAuto:
       setBits(&remote_state[16], kHighNibble, kNibbleSize,
@@ -668,9 +670,9 @@ uint8_t IRPanasonicAc::convertMode(const stdAc::opmode_t mode) {
 uint8_t IRPanasonicAc::convertFan(const stdAc::fanspeed_t speed) {
   switch (speed) {
     case stdAc::fanspeed_t::kMin:    return kPanasonicAcFanMin;
-    case stdAc::fanspeed_t::kLow:    return kPanasonicAcFanMin + 1;
-    case stdAc::fanspeed_t::kMedium: return kPanasonicAcFanMin + 2;
-    case stdAc::fanspeed_t::kHigh:   return kPanasonicAcFanMin + 3;
+    case stdAc::fanspeed_t::kLow:    return kPanasonicAcFanLow;
+    case stdAc::fanspeed_t::kMedium: return kPanasonicAcFanMed;
+    case stdAc::fanspeed_t::kHigh:   return kPanasonicAcFanHigh;
     case stdAc::fanspeed_t::kMax:    return kPanasonicAcFanMax;
     default:                         return kPanasonicAcFanAuto;
   }
@@ -723,9 +725,9 @@ stdAc::opmode_t IRPanasonicAc::toCommonMode(const uint8_t mode) {
 stdAc::fanspeed_t IRPanasonicAc::toCommonFanSpeed(const uint8_t spd) {
   switch (spd) {
     case kPanasonicAcFanMax:     return stdAc::fanspeed_t::kMax;
-    case kPanasonicAcFanMin + 3: return stdAc::fanspeed_t::kHigh;
-    case kPanasonicAcFanMin + 2: return stdAc::fanspeed_t::kMedium;
-    case kPanasonicAcFanMin + 1: return stdAc::fanspeed_t::kLow;
+    case kPanasonicAcFanHigh:    return stdAc::fanspeed_t::kHigh;
+    case kPanasonicAcFanMed:     return stdAc::fanspeed_t::kMedium;
+    case kPanasonicAcFanLow:     return stdAc::fanspeed_t::kLow;
     case kPanasonicAcFanMin:     return stdAc::fanspeed_t::kMin;
     default:                     return stdAc::fanspeed_t::kAuto;
   }
@@ -791,9 +793,9 @@ String IRPanasonicAc::toString(void) {
   result += addModeToString(getMode(), kPanasonicAcAuto, kPanasonicAcCool,
                             kPanasonicAcHeat, kPanasonicAcDry, kPanasonicAcFan);
   result += addTempToString(getTemp());
-  result += addFanToString(getFan(), kPanasonicAcFanMax, kPanasonicAcFanMin,
-                           kPanasonicAcFanAuto, kPanasonicAcFanAuto,
-                           kPanasonicAcFanMed);
+  result += addFanToString(getFan(), kPanasonicAcFanHigh, kPanasonicAcFanLow,
+                           kPanasonicAcFanAuto, kPanasonicAcFanMin,
+                           kPanasonicAcFanMed, kPanasonicAcFanMax);
   result += addSwingVToString(getSwingVertical(), kPanasonicAcSwingVAuto,
                               kPanasonicAcSwingVHighest,
                               kPanasonicAcSwingVHigh,

src/ir_Panasonic.h
@@ -52,7 +52,9 @@ const uint8_t kPanasonicAcCool = 3;  // 0b011
 const uint8_t kPanasonicAcHeat = 4;  // 0b010
 const uint8_t kPanasonicAcFan = 6;   // 0b110
 const uint8_t kPanasonicAcFanMin = 0;
+const uint8_t kPanasonicAcFanLow = 1;
 const uint8_t kPanasonicAcFanMed = 2;
+const uint8_t kPanasonicAcFanHigh = 3;
 const uint8_t kPanasonicAcFanMax = 4;
 const uint8_t kPanasonicAcFanAuto = 7;
 const uint8_t kPanasonicAcFanDelta = 3;

test/IRac_test.cpp
@@ -1243,7 +1243,7 @@ TEST(TestIRac, Panasonic) {
   ASSERT_TRUE(IRAcUtils::decodeToState(&ac._irsend.capture, &r, &p));
 
   char expected_dke[] =
-      "Model: 3 (DKE), Power: On, Mode: 3 (Cool), Temp: 18C, Fan: 4 (High), "
+      "Model: 3 (DKE), Power: On, Mode: 3 (Cool), Temp: 18C, Fan: 4 (Maximum), "
       "Swing(V): 2 (High), Swing(H): 6 (Middle), "
       "Quiet: Off, Powerful: On, Ion: On, "
       "Clock: 19:17, On Timer: Off, Off Timer: Off";

test/ir_Panasonic_test.cpp
@@ -665,15 +665,15 @@ TEST(TestIRPanasonicAcClass, SetAndGetFan) {
   EXPECT_EQ(kPanasonicAcFanMin, ac.getFan());
   ac.setFan(kPanasonicAcFanMin - 1);
   EXPECT_EQ(kPanasonicAcFanAuto, ac.getFan());
+  ac.setFan(kPanasonicAcFanLow);
+  EXPECT_EQ(kPanasonicAcFanLow, ac.getFan());
   ac.setFan(kPanasonicAcFanMed);
   EXPECT_EQ(kPanasonicAcFanMed, ac.getFan());
-  ac.setFan(kPanasonicAcFanMin + 1);
-  EXPECT_EQ(kPanasonicAcFanAuto, ac.getFan());
+  ac.setFan(kPanasonicAcFanHigh);
+  EXPECT_EQ(kPanasonicAcFanHigh, ac.getFan());
   ac.setFan(kPanasonicAcFanMax);
   EXPECT_EQ(kPanasonicAcFanMax, ac.getFan());
   ac.setFan(kPanasonicAcFanMax + 1);
-  EXPECT_EQ(kPanasonicAcFanAuto, ac.getFan());
-  ac.setFan(kPanasonicAcFanMax - 1);
   EXPECT_EQ(kPanasonicAcFanAuto, ac.getFan());
 }
 
@@ -788,14 +788,14 @@ TEST(TestIRPanasonicAcClass, HumanReadable) {
   ac.setPowerful(true);
   EXPECT_EQ(
       "Model: 4 (JKE), Power: On, Mode: 4 (Heat), Temp: 30C, "
-      "Fan: 4 (High), Swing(V): 15 (Auto), Quiet: Off, "
+      "Fan: 4 (Maximum), Swing(V): 15 (Auto), Quiet: Off, "
       "Powerful: On, Clock: 00:00, On Timer: Off, Off Timer: Off",
       ac.toString());
   ac.setQuiet(true);
   ac.setModel(kPanasonicLke);
   EXPECT_EQ(
       "Model: 1 (LKE), Power: Off, Mode: 4 (Heat), Temp: 30C, "
-      "Fan: 4 (High), Swing(V): 15 (Auto), "
+      "Fan: 4 (Maximum), Swing(V): 15 (Auto), "
       "Swing(H): 6 (Middle), Quiet: On, Powerful: Off, "
       "Clock: 00:00, On Timer: 00:00, Off Timer: Off",
       ac.toString());
@@ -803,7 +803,7 @@ TEST(TestIRPanasonicAcClass, HumanReadable) {
   ac.setSwingHorizontal(kPanasonicAcSwingHRight);
   EXPECT_EQ(
       "Model: 3 (DKE), Power: Off, Mode: 4 (Heat), Temp: 30C, "
-      "Fan: 4 (High), Swing(V): 15 (Auto), "
+      "Fan: 4 (Maximum), Swing(V): 15 (Auto), "
       "Swing(H): 11 (Right), Quiet: On, Powerful: Off, Ion: Off, "
       "Clock: 00:00, On Timer: Off, Off Timer: Off",
       ac.toString());