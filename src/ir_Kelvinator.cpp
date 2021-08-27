2472119384cd1a391611befeebbe7ceba622fe57
crankyoldgit <david@xyzzy.com.au>
2021-08-27 20:47:43 +1000

    Cause a doxygen error to test the action

--- src/ir_Kelvinator.cpp
+++ src/ir_Kelvinator.cpp
@@ -104,7 +104,6 @@ void IRsend::sendKelvinator(const unsigned char data[], const uint16_t nbytes,
 
 /// Class constructor
 /// @param[in] pin GPIO to be used when sending.
-/// @param[in] inverted Is the output signal to be inverted?
 /// @param[in] use_modulation Is frequency modulation to be used?
 IRKelvinatorAC::IRKelvinatorAC(const uint16_t pin, const bool inverted,
                                const bool use_modulation)
