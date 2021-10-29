crankyoldgit/IRremoteESP8266

commit 10706fc26acf0f72c8ef59bad855f3b5e901ab7a

Re-work "Fan Only" strings & matching. (#1610)
* Improve compatiblity and human clarity.
* Add note about why it exists in the first place.

Replaces PR #1604

src/IRac.cpp
@@ -3076,8 +3076,14 @@ stdAc::opmode_t IRac::strToOpmode(const char *str,
            !strcasecmp(str, "DEHUMIDIFY"))
     return stdAc::opmode_t::kDry;
   else if (!strcasecmp(str, kFanStr) ||
-           !strcasecmp(str, "FANONLY") ||
-           !strcasecmp(str, kFanOnlyStr))
+          // The following Fans strings with "only" are required to help with
+          // HomeAssistant & Google Home Climate integration.
+          // For compatibility only.
+          // Ref: https://www.home-assistant.io/integrations/google_assistant/#climate-operation-modes
+           !strcasecmp(str, kFanOnlyStr) ||
+           !strcasecmp(str, kFan_OnlyStr) ||
+           !strcasecmp(str, kFanOnlyWithSpaceStr) ||
+           !strcasecmp(str, kFanOnlyNoSpaceStr))
     return stdAc::opmode_t::kFan;
   else
     return def;

src/IRtext.cpp
@@ -107,9 +107,16 @@ const PROGMEM char* kAutomaticStr = D_STR_AUTOMATIC;  ///< "Automatic"
 const PROGMEM char* kManualStr = D_STR_MANUAL;  ///< "Manual"
 const PROGMEM char* kCoolStr = D_STR_COOL;  ///< "Cool"
 const PROGMEM char* kHeatStr = D_STR_HEAT;  ///< "Heat"
-const PROGMEM char* kFanStr = D_STR_FAN;  ///< "Fan"
 const PROGMEM char* kDryStr = D_STR_DRY;  ///< "Dry"
-const PROGMEM char* kFanOnlyStr = D_STR_FANONLY;  ///< "fan_only"
+const PROGMEM char* kFanStr = D_STR_FAN;  ///< "Fan"
+// The following Fans strings with "only" are required to help with
+// HomeAssistant & Google Home Climate integration. For compatibility only.
+// Ref: https://www.home-assistant.io/integrations/google_assistant/#climate-operation-modes
+const PROGMEM char* kFanOnlyStr = D_STR_FANONLY;  ///< "fan-only"
+const PROGMEM char* kFan_OnlyStr = D_STR_FAN_ONLY;  ///< "fan_only" (legacy use)
+const PROGMEM char* kFanOnlyWithSpaceStr = D_STR_FANSPACEONLY;  ///< "Fan Only"
+const PROGMEM char* kFanOnlyNoSpaceStr = D_STR_FANONLYNOSPACE;  ///< "FanOnly"
+
 const PROGMEM char* kRecycleStr = D_STR_RECYCLE;  ///< "Recycle"
 
 const PROGMEM char* kMaxStr = D_STR_MAX;  ///< "Max"

src/IRtext.h
@@ -51,7 +51,10 @@ extern const char* kEconoToggleStr;
 extern const char* kEyeAutoStr;
 extern const char* kEyeStr;
 extern const char* kFalseStr;
+extern const char* kFanOnlyNoSpaceStr;
+extern const char* kFan_OnlyStr;
 extern const char* kFanOnlyStr;
+extern const char* kFanOnlyWithSpaceStr;
 extern const char* kFanStr;
 extern const char* kFastStr;
 extern const char* kFilterStr;

src/locale/defaults.h
@@ -305,8 +305,20 @@
 #define D_STR_FAN "Fan"
 #endif  // D_STR_FAN
 #ifndef D_STR_FANONLY
-#define D_STR_FANONLY "fan_only"
+#define D_STR_FANONLY "fan-only"
 #endif  // D_STR_FANONLY
+#ifndef D_STR_FAN_ONLY
+#define D_STR_FAN_ONLY "fan_only"
+#endif  // D_STR_FAN_ONLY
+#ifndef D_STR_ONLY
+#define D_STR_ONLY "Only"
+#endif  // D_STR_ONLY
+#ifndef D_STR_FANSPACEONLY
+#define D_STR_FANSPACEONLY D_STR_FAN " " D_STR_ONLY
+#endif  // D_STR_FANSPACEONLY
+#ifndef D_STR_FANONLYNOSPACE
+#define D_STR_FANONLYNOSPACE D_STR_FAN D_STR_ONLY
+#endif  // D_STR_FANONLYNOSPACE
 #ifndef D_STR_DRY
 #define D_STR_DRY "Dry"
 #endif  // D_STR_DRY

commit 49bfe4c404679cdd528e73981e3f31a6ded9efd2

Use the correct string for Fan mode in Home Assistant. (#1657)
Home assistant needs the text for "Fan" mode to be "fan_only". i.e. the Underscore is important.
Problem was introduced in #1610

src/IRac.cpp
@@ -3438,7 +3438,7 @@ String IRac::opmodeToString(const stdAc::opmode_t mode, const bool ha) {
     case stdAc::opmode_t::kCool: return kCoolStr;
     case stdAc::opmode_t::kHeat: return kHeatStr;
     case stdAc::opmode_t::kDry:  return kDryStr;
-    case stdAc::opmode_t::kFan:  return ha ? kFanOnlyStr : kFanStr;
+    case stdAc::opmode_t::kFan:  return ha ? kFan_OnlyStr : kFanStr;
     default:                     return kUnknownStr;
   }
 }

src/IRtext.cpp
@@ -140,7 +140,7 @@ IRTEXT_CONST_STRING(kFanStr, D_STR_FAN);  ///< "Fan"
 // HomeAssistant & Google Home Climate integration. For compatibility only.
 // Ref: https://www.home-assistant.io/integrations/google_assistant/#climate-operation-modes
 IRTEXT_CONST_STRING(kFanOnlyStr, D_STR_FANONLY);  ///< "fan-only"
-IRTEXT_CONST_STRING(kFan_OnlyStr, D_STR_FAN_ONLY);  ///< "fan_only" (legacy)
+IRTEXT_CONST_STRING(kFan_OnlyStr, D_STR_FAN_ONLY);  ///< "fan_only" (HA/legacy)
 IRTEXT_CONST_STRING(kFanOnlyWithSpaceStr, D_STR_FANSPACEONLY);  ///< "Fan Only"
 IRTEXT_CONST_STRING(kFanOnlyNoSpaceStr, D_STR_FANONLYNOSPACE);  ///< "FanOnly"
 

test/IRac_test.cpp
@@ -2468,7 +2468,7 @@ TEST(TestIRac, opmodeToString) {
   EXPECT_EQ("UNKNOWN", IRac::opmodeToString((stdAc::opmode_t)500));
   // Home Assistant/Google Home differences.
   EXPECT_EQ("Fan", IRac::opmodeToString(stdAc::opmode_t::kFan, false));
-  EXPECT_EQ("fan-only", IRac::opmodeToString(stdAc::opmode_t::kFan, true));
+  EXPECT_EQ("fan_only", IRac::opmodeToString(stdAc::opmode_t::kFan, true));
   EXPECT_EQ("Fan", IRac::opmodeToString(stdAc::opmode_t::kFan));  // Default
 }
 