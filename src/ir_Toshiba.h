eed3ead7eab830128c60f6c6d43cb9cc6d265435
crankyoldgit <david@xyzzy.com.au>
2021-11-29 23:00:56 +1000

    Add supported A/C model number

--- src/ir_Toshiba.h
+++ src/ir_Toshiba.h
@@ -20,6 +20,7 @@
 //   Brand: Toshiba,  Model: WH-UB03NJ remote
 //   Brand: Toshiba,  Model: RAS-2558V A/C
 //   Brand: Toshiba,  Model: WH-TA01JE remote
+//   Brand: Toshiba,  Model: RAS-25SKVP2-ND A/C
 //   Brand: Carrier,  Model: 42NQV060M2 / 38NYV060M2 A/C
 //   Brand: Carrier,  Model: 42NQV050M2 / 38NYV050M2 A/C
 //   Brand: Carrier,  Model: 42NQV035M2 / 38NYV035M2 A/C
