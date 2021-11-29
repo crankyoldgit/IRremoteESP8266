999a527e8d46440b5c65f940480ab19558eab366
crankyoldgit <david@xyzzy.com.au>
2021-11-29 22:52:41 +1000

    Add supported remote

--- src/ir_Toshiba.h
+++ src/ir_Toshiba.h
@@ -19,6 +19,7 @@
 //   Brand: Toshiba,  Model: WC-L03SE
 //   Brand: Toshiba,  Model: WH-UB03NJ remote
 //   Brand: Toshiba,  Model: RAS-2558V A/C
+//   Brand: Toshiba,  Model: WH-TA01JE remote
 //   Brand: Carrier,  Model: 42NQV060M2 / 38NYV060M2 A/C
 //   Brand: Carrier,  Model: 42NQV050M2 / 38NYV050M2 A/C
 //   Brand: Carrier,  Model: 42NQV035M2 / 38NYV035M2 A/C
