441c52296869c3b4e0a18b99cd4db2ec618f74b5
Ton Huisman <tonhu2004@hotmail.com>
2022-08-16 11:52:24 +0200

    Move Protocol string-selection to macro, in new file IRmacros.h

---
+++ src/IRmacros.h
@@ -0,0 +1,26 @@
+#ifndef IRMACROS_H_
+#define IRMACROS_H_
+
+/**
+ * COND() Set of macros to facilitate single-line conditional compilation
+ * argument checking.
+ * Found here: https://www.reddit.com/r/C_Programming/comments/ud3xrv/
+ * conditional_preprocessor_macro_anyone/
+ * 
+ * Usage:
+ * COND(<define_to_test>[||/&&<more_define>...], <true_result>, <false_result)
+ */
+#define NOTHING
+#define EXPAND(...) __VA_ARGS__
+#define STUFF_P(a, ...) __VA_OPT__(a)
+#define STUFF(...) STUFF_P(__VA_ARGS__)
+#define VA_TEST_P(a,...) __VA_OPT__(NO)##THING
+#define VA_TEST(...) VA_TEST_P(__VA_ARGS__)
+#define NEGATE(a) VA_TEST(a,a)
+#define COND_P(cond,a,b) STUFF(a,cond)STUFF(b,NEGATE(cond))
+#define COND(cond,a,b) EXPAND(COND_P(cond, a, b))
+/**
+ * end of COND() set of macros
+ */
+
+#endif  // IRMACROS_H_
