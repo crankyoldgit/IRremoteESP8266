6d3dbd239ecd1e3fad328863f1249c33c3ad1858
Ton Huisman <tonhu2004@hotmail.com>
2022-09-16 21:26:32 +0200

    [Bugfix] Handle gcc unsupported __VA_OPT___ macro

--- src/IRmacros.h
+++ src/IRmacros.h
@@ -6,6 +6,29 @@
 /// @file IRmacros.h
 
 /**
+ * PP_NARG() macro get number of arguments provided
+ * Source:
+ * http://jonjagger.blogspot.com/2010/11/c-macro-magic-ppnarg.html
+ * ('base' version)
+ */
+/// @cond TEST
+#define PP_NARG(...) \
+    PP_NARG_(__VA_ARGS__, PP_RSEQ_N())
+
+#define PP_NARG_(...) \
+    PP_ARG_N(__VA_ARGS__)
+
+#define PP_ARG_N( \
+    _1, _2, _3, _4, _5, _6, N, ...)   (N)
+
+#define PP_RSEQ_N() \
+    6,5,4,3,2,1,0
+/// @endcond
+/**
+ * PP_NARG() end
+ */
+
+/**
  * COND() Set of macros to facilitate single-line conditional compilation
  * argument checking.
  * Found here: https://www.reddit.com/r/C_Programming/comments/ud3xrv/
@@ -13,8 +36,13 @@
  * 
  * Usage:
  * COND(<define_to_test>[||/&&<more_define>...], <true_result>, <false_result>)
+ * 
+ * NB: If __VA_OPT__ macro not supported, the <true_result> will be expanded!
  */
 /// @cond TEST
+#if PP_NARG(__VA_OPT__(,)) != 2
+#define COND(cond, a, b) a
+#else
 #define NOTHING
 #define EXPAND(...) __VA_ARGS__
 #define STUFF_P(a, ...) __VA_OPT__(a)
@@ -24,6 +52,7 @@
 #define NEGATE(a) VA_TEST(a, a)
 #define COND_P(cond, a, b) STUFF(a, cond)STUFF(b, NEGATE(cond))
 #define COND(cond, a, b) EXPAND(COND_P(cond, a, b))
+#endif
 /// @endcond
 /**
  * end of COND() set of macros
