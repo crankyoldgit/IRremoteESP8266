6229d150f3c23bac832b9f4911436628b752f0c7
Ton Huisman <tonhu2004@hotmail.com>
2022-09-17 10:57:47 +0200

    [Bugfix] Handle gcc unsupported __VA_OPT___ macro, fix linter contradiction

--- src/IRmacros.h
+++ src/IRmacros.h
@@ -14,7 +14,7 @@
 /// @cond TEST
 #define PP_THIRD_ARG(a, b, c, ...) c
 #define VA_OPT_SUPPORTED_I(...) \
-            PP_THIRD_ARG(__VA_OPT__(, ), true, false, false)
+            PP_THIRD_ARG(__VA_OPT__(, false), true, false, false)
 #define VA_OPT_SUPPORTED VA_OPT_SUPPORTED_I(?)
 /// @endcond
 /**
