349f1fb7bb2bc1a9ac5329cf07c35273b6a287b0
Ton Huisman <tonhu2004@hotmail.com>
2022-09-17 10:32:21 +0200

    [Bugfix] Handle gcc unsupported __VA_OPT___ macro, fix doxygen warnings

--- src/IRmacros.h
+++ src/IRmacros.h
@@ -11,9 +11,11 @@
  * portably-detect-va-opt-support
  * (answer by cpplearner)
  */
+/// @cond TEST
 #define PP_THIRD_ARG(a, b, c, ...) c
 #define VA_OPT_SUPPORTED_I(...) PP_THIRD_ARG(__VA_OPT__(, ), true, false, )
 #define VA_OPT_SUPPORTED VA_OPT_SUPPORTED_I(?)
+/// @endcond
 /**
  * VA_OPT_SUPPORTED end
  */
