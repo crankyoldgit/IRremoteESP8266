c35868dffb9aa18e0288bda766d4a744fc061ada
Guillaume Giraudon <guillaume@axiomaudio.com>
2025-05-09 18:15:11 -0400

    Complete implementation of Bryston protocol

--- src/ir_Bryston.cpp
+++ src/ir_Bryston.cpp
@@ -1,5 +1,9 @@
+// Copyright 2025 Guillaume Giraudon - Colquhoun Audio Laboratories
+
+
 /// @file
-/// @brief Bryston
+/// @brief Support for Bryston Protocols.
+/// @note Currently only tested BP19/BR20 but should work for all Bryston Products.
 
 
 // Supports:
@@ -12,36 +16,32 @@
 
 
 // Constants
-const uint16_t kBrystonTick = 315;
-const uint16_t kBrystonHdrMarkTicks = 1;
-const uint16_t kBrystonHdrMark = kBrystonHdrMarkTicks * kBrystonTick;
-const uint16_t kBrystonHdrSpaceTicks = 3;
-const uint16_t kBrystonHdrSpace = kBrystonHdrSpaceTicks * kBrystonTick;
-const uint16_t kBrystonBitMarkTicks = 6;
-const uint16_t kBrystonBitMark = kBrystonBitMarkTicks * kBrystonTick;
-const uint16_t kBrystonOneSpaceTicks = 1;
-const uint16_t kBrystonOneSpace = kBrystonOneSpaceTicks * kBrystonTick;
-const uint16_t kBrystonZeroSpaceTicks = 6;
-const uint16_t kBrystonZeroSpace = kBrystonZeroSpaceTicks * kBrystonTick;
-const uint16_t kBrystonMinGapTicks = 18;
-const uint16_t kBrystonMinGap = kBrystonMinGapTicks * kBrystonTick;
+const uint16_t kBrystonTicks = 315;  // Number of bits in a Bryston message.
+const uint16_t kBrystonHdrMark = 0;
+const uint16_t kBrystonHdrSpace = 0;
+const uint16_t kBrystonOneMark = 6 * kBrystonTicks;
+const uint16_t kBrystonOneSpace = 1 * kBrystonTicks;
+const uint16_t kBrystonZeroMark = 1 * kBrystonTicks;
+const uint16_t kBrystonZeroSpace = 6 * kBrystonTicks;
+const uint16_t kBrystonMinGap = 0;
+const uint16_t kBrystonFooterMark = 0;
 
 #if SEND_BRYSTON
 /// Send a Bryston formatted message.
-/// Status: STABLE / Working.
 /// @param[in] data The message to be sent.
 /// @param[in] nbits The number of bits of message to be sent.
 /// @param[in] repeat The number of times the command is to be repeated.
 void IRsend::sendBryston(uint64_t data, uint16_t nbits, uint16_t repeat) {
-  sendGeneric(kBrystonHdrMark, kBrystonHdrSpace, kBrystonBitMark, kBrystonOneSpace,
-              kBrystonBitMark, kBrystonZeroSpace, kBrystonBitMark, kBrystonMinGap, data,
+  Serial.printf("Sending Bryston: %016llX\n", data);
+
+  sendGeneric(kBrystonHdrMark, kBrystonHdrSpace, kBrystonOneMark, kBrystonOneSpace,
+              kBrystonZeroMark, kBrystonZeroSpace, kBrystonFooterMark, kBrystonMinGap, data,
               nbits, 38, true, repeat, 33);
 }
 #endif  // SEND_Bryston
 
 #if DECODE_BRYSTON
 /// Decode the supplied Bryston message.
-/// Status: STABLE / Working.
 /// @param[in,out] results Ptr to the data to decode & where to store the result
 /// @param[in] offset The starting index to use when attempting to decode the
 ///   raw data. Typically/Defaults to kStartOffset.
@@ -58,9 +58,9 @@ bool IRrecv::decodeBryston(decode_results *results, uint16_t offset,
   if (!matchGeneric(results->rawbuf + offset, &data,
                     results->rawlen - offset, nbits,
                     kBrystonHdrMark, kBrystonHdrSpace,
-                    kBrystonBitMark, kBrystonOneSpace,
-                    kBrystonBitMark, kBrystonZeroSpace,
-                    kBrystonBitMark, kBrystonMinGap, true)) return false;
+                    kBrystonOneMark, kBrystonOneSpace,
+                    kBrystonZeroMark, kBrystonZeroSpace,
+                    kBrystonFooterMark, kBrystonMinGap, true)) return false;
   // Success
   results->bits = nbits;
   results->value = data;
